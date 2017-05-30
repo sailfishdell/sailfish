34cae076e185ebefd6b26c8cdd97ed2456c05d62
Michael E Brown <michael_e_brown@dell.com>
2017-05-30 15:42:23 -0500

    Add logging middleware. Prep for adding metrics middleware.
    Signed-off-by: Michael E Brown <michael_e_brown@dell.com>

---
+++ src/logging.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "time"
+    "net/http"
+
+    "github.com/go-kit/kit/log"
+)
+
+type loggingService struct {
+    logger log.Logger
+    RedfishService
+}
+
+// NewLoggingService returns a new instance of a logging Service.
+func NewLoggingService(logger log.Logger, s RedfishService) RedfishService {
+    return &loggingService{logger, s}
+}
+
+func (s *loggingService) GetRedfish(r *http.Request) (ret []byte, err error) {
+    defer func(begin time.Time) {
+        s.logger.Log(
+            "method", "GET",
+            "took", time.Since(begin),
+            "err", err,
+        )
+    }(time.Now())
+    return s.RedfishService.GetRedfish(r)
+}
+
--- src/main.go
+++ src/main.go
@@ -21,9 +21,33 @@ func main() {
 	logger = log.NewLogfmtLogger(os.Stderr)
 	logger = log.With(logger, "listen", *listen, "caller", log.DefaultCaller)
 
+/*
+    // START prometheus metrics
+    fieldKeys := []string{"method", "error"}
+    requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+        Namespace: "redfish_group",
+        Subsystem: "redfish_service",
+        Name:      "request_count",
+        Help:      "Number of requests received.",
+    }, fieldKeys)
+    requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+        Namespace: "redfish_group",
+        Subsystem: "redfish_service",
+        Name:      "request_latency_microseconds",
+        Help:      "Total duration of requests in microseconds.",
+    }, fieldKeys)
+    countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+        Namespace: "redfish_group",
+        Subsystem: "redfish_service",
+        Name:      "count_result",
+        Help:      "The result of each count method.",
+    }, []string{})
+    // END prometheus metrics
+*/
+
 	var svc RedfishService
 	svc = NewService(*rootpath, logger)
-
+    svc = NewLoggingService(log.With(logger, "foo", "bar"), svc)
 	redfishHandler := httptransport.NewServer(
 		makeRedfishEndpoint(svc),
 		decodeRedfishRequest,
