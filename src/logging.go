ad3bf6f644ea128333ba1b2872c28cbae944f6d1
Michael E Brown <michael_e_brown@dell.com>
2017-05-30 16:02:58 -0500

    go fmt
    Signed-off-by: Michael E Brown <michael_e_brown@dell.com>

--- src/logging.go
+++ src/logging.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-    "time"
-    "net/http"
+	"net/http"
+	"time"
 
-    "github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log"
 )
 
 type loggingService struct {
-    logger log.Logger
-    RedfishService
+	logger log.Logger
+	RedfishService
 }
 
 // NewLoggingService returns a new instance of a logging Service.
 func NewLoggingService(logger log.Logger, s RedfishService) RedfishService {
-    return &loggingService{logger, s}
+	return &loggingService{logger, s}
 }
 
 func (s *loggingService) GetRedfish(r *http.Request) (ret []byte, err error) {
-    defer func(begin time.Time) {
-        s.logger.Log(
-            "method", "GET",
-            "took", time.Since(begin),
-            "err", err,
-        )
-    }(time.Now())
-    return s.RedfishService.GetRedfish(r)
+	defer func(begin time.Time) {
+		s.logger.Log(
+			"method", "GET",
+			"took", time.Since(begin),
+			"err", err,
+		)
+	}(time.Now())
+	return s.RedfishService.GetRedfish(r)
 }
-
--- src/main.go
+++ src/main.go
@@ -6,12 +6,11 @@ import (
 	"os"
 
 	"github.com/go-kit/kit/log"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/gorilla/mux"
-    stdprometheus "github.com/prometheus/client_golang/prometheus"
-    kitprometheus "github.com/go-kit/kit/metrics/prometheus"
-       "github.com/prometheus/client_golang/prometheus/promhttp"
-
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -27,24 +26,24 @@ func main() {
 
 	var svc RedfishService
 	svc = NewService(*rootpath, logger)
-    svc = NewLoggingService(log.With(logger, "foo", "bar"), svc)
+	svc = NewLoggingService(log.With(logger, "foo", "bar"), svc)
 
-    fieldKeys := []string{"method"}
-    svc = NewInstrumentingService(
-        kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
-            Namespace: "redfish",
-            Subsystem: "redfish_service",
-            Name:      "request_count",
-            Help:      "Number of requests received.",
-        }, fieldKeys),
-        kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-            Namespace: "redfish",
-            Subsystem: "redfish_service",
-            Name:      "request_latency_microseconds",
-            Help:      "Total duration of requests in microseconds.",
-        }, fieldKeys),
-        svc,
-    )
+	fieldKeys := []string{"method"}
+	svc = NewInstrumentingService(
+		kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "redfish",
+			Subsystem: "redfish_service",
+			Name:      "request_count",
+			Help:      "Number of requests received.",
+		}, fieldKeys),
+		kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+			Namespace: "redfish",
+			Subsystem: "redfish_service",
+			Name:      "request_latency_microseconds",
+			Help:      "Total duration of requests in microseconds.",
+		}, fieldKeys),
+		svc,
+	)
 
 	redfishHandler := httptransport.NewServer(
 		makeRedfishEndpoint(svc),
@@ -56,7 +55,7 @@ func main() {
 	r.PathPrefix("/redfish/v1/").Handler(http.StripPrefix("/redfish/v1/", redfishHandler))
 
 	http.Handle("/", r)
-    http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/metrics", promhttp.Handler())
 
 	logger.Log("msg", "HTTP", "addr", *listen)
 	logger.Log("err", http.ListenAndServe(*listen, nil))
