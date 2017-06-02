847aaf9ecd1f1c3b5f999b64e7f0a0e8ac320ba1
Michael E Brown <michael_e_brown@dell.com>
2017-06-02 16:31:03 -0500

    Add notes and reference links for the go tools that are useful and prereqs for the pre-commit-stuff.sh
    Signed-off-by: Michael E Brown <michael_e_brown@dell.com>

--- pre-commit-stuff.sh
+++ pre-commit-stuff.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
-golint commands/mockserver/  src/mockbackend/  src/redfishserver/
+# list of tools
+# https://dominik.honnef.co/posts/2014/12/go-tools
+#
+# go get https://github.com/golang/tools
+# go get golang.org/cmd/gofmt
+# go get github.com/golang/lint/golint
+# go get github.com/kisielk/errcheck
+# go get github.com/nsf/gocode
+# go get code.google.com/p/rog-go/exp/cmd/godef
+#   - vim-godef: https://github.com/dgryski/vim-godef
+# go get golang.org/x/tools/cmd/gorename
+# go get github.com/kisielk/godepgraph
 
 go fmt  \
+    github.com/superchalupa/go-redfish/commands/mockserver  \
+    github.com/superchalupa/go-redfish/src/redfishserver/   \
+    github.com/superchalupa/go-redfish/src/mockbackend/
+
+go vet  \
+    github.com/superchalupa/go-redfish/commands/mockserver  \
+    github.com/superchalupa/go-redfish/src/redfishserver/   \
+    github.com/superchalupa/go-redfish/src/mockbackend/
+
+golint commands/mockserver/  src/mockbackend/  src/redfishserver/
+
+errcheck  \
     github.com/superchalupa/go-redfish/commands/mockserver  \
     github.com/superchalupa/go-redfish/src/redfishserver/   \
     github.com/superchalupa/go-redfish/src/mockbackend/
