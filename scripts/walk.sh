a6b5d584d8c721c4319e6f1886d3a254e85da23b
Michael E Brown <michael_e_brown@dell.com>
2017-06-14 09:32:34 -0500

    Add stats output for loops
    Signed-off-by: Michael E Brown <michael_e_brown@dell.com>

--- scripts/walk.sh
+++ scripts/walk.sh
@@ -7,7 +7,12 @@ scriptdir=$(cd $(dirname $0); pwd)
 
 HOST=${HOST:-localhost}
 PORT=${PORT:-8080}
-BASE=http://${HOST}:${PORT}
+if [ "${PORT}" = "443" -o "${PORT}" = "8443" ]; then
+    PROTO=https
+else
+    PROTO=http
+fi
+BASE=${PROTO}://${HOST}:${PORT}
 START_URL="/redfish/v1/"
 
 if [ -n "$TOKEN" ]; then
@@ -17,6 +22,7 @@ fi
 echo $START_URL | sort |uniq > URLS-to-visit.txt
 rm -f URLS-visited.txt
 
+LOOPS=1
 while ! diff -u URLS-to-visit.txt URLS-visited.txt
 do
     for url in $(cat URLS-to-visit.txt)
@@ -31,6 +37,9 @@ do
     mv URLS-to-visit-new.txt URLS-to-visit.txt
     cat URLS-visited.txt | sort | uniq > URLS-visited-new.txt
     mv URLS-visited-new.txt  URLS-visited.txt
+    LOOPS=$(( LOOPS + 1 ))
 done
 
 time curl $AUTH_HEADER -s -L -w"\nTotal request time: %{time_total} seconds\n" $(cat URLS-visited.txt | perl -n -e "print '${BASE}' . \$_" )
+
+echo "Took $LOOPS loops to collect the URL list"
