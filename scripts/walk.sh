5b963cd144e345803d3a7d33f83e5fd3188aa5fc
Michael E Brown <michael_e_brown@dell.com>
2017-06-19 16:21:55 -0500

    Add ability to scan idrac/ec tree.
    Signed-off-by: Michael E Brown <michael_e_brown@dell.com>

--- scripts/walk.sh
+++ scripts/walk.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+unset HTTP_PROXY HTTPS_PROXY http_proxy https_proxy
+
 scriptdir=$(cd $(dirname $0); pwd)
 
 HOST=${HOST:-localhost}
@@ -15,22 +17,30 @@ fi
 BASE=${PROTO}://${HOST}:${PORT}
 START_URL="/redfish/v1/"
 
+AUTH_HEADER="foo: bar"
 if [ -n "$TOKEN" ]; then
-    AUTH_HEADER="-H 'Authentication: bearer $TOKEN'"
+    AUTH_HEADER="Authorization: Bearer $TOKEN"
 fi
 
 echo $START_URL | sort |uniq > URLS-to-visit.txt
 rm -f URLS-visited.txt
+rm -rf out/
+mkdir out
 
 LOOPS=0
-while ! diff -u URLS-to-visit.txt URLS-visited.txt
+TRIEDONE=1
+while [ $TRIEDONE -eq 1 ]
 do
+    TRIEDONE=0
     for url in $(cat URLS-to-visit.txt)
     do
         if grep -q "^${url}\$" URLS-visited.txt; then
             continue
         fi
-        curl $AUTH_HEADER -s -L ${BASE}${url} | jq -r 'recurse (.[]?) | objects | select(has("@odata.id")) | .["@odata.id"]' >> URLS-to-visit.txt
+        TRIEDONE=1
+        OUTFILE=out/$( echo -n ${url} | perl -p -e 's/[^a-zA-Z0-9]/_/g;' ).json
+        curl -H "$AUTH_HEADER" -s -L ${BASE}${url}  > $OUTFILE || continue
+        cat $OUTFILE | jq -r 'recurse (.[]?) | objects | select(has("@odata.id")) | .["@odata.id"]' >> URLS-to-visit.txt ||:
         echo $url >> URLS-visited.txt
     done
     cat URLS-to-visit.txt | sort | uniq > URLS-to-visit-new.txt
@@ -40,6 +50,6 @@ do
     LOOPS=$(( LOOPS + 1 ))
 done
 
-time curl $AUTH_HEADER -s -L -w"\nTotal request time: %{time_total} seconds\n" $(cat URLS-visited.txt | perl -n -e "print '${BASE}' . \$_" )
+time curl -H "$AUTH_HEADER" -s -L -w"\nTotal request time: %{time_total} seconds for url: %{url_effective}\n" $(cat URLS-visited.txt | perl -n -e "print '${BASE}' . \$_" ) | tee out/entire-tree.txt
 
 echo "Took $LOOPS loops to collect the URL list"
