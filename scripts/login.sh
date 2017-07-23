1e7f3ea10d29d93876d1fc973aacae525f7992f2
Michael Brown <mebrown@michaels-house.net>
2017-07-23 00:34:15 -0500

    Update login to also set variable with sessionuri
    Signed-off-by: Michael Brown <mebrown@michaels-house.net>

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,20 +1,39 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 username=$1
 password=$2
+headersfile=$(mktemp /tmp/headers-XXXXXX)
+trap 'rm $headersfile' EXIT QUIT HUP INT ERR
 
-export X_AUTH_TOKEN=$(curl -v http://localhost:8080/redfish/v1/SessionService/Sessions -X POST -d "{\"UserName\": \"${username}\", \"Password\": \"${password}\"}" 2>&1 | grep X-Auth-Token | cut -d: -f2 | perl -p -e 's/\r//g;')
+RESPONSE_HEADERS=$(curl -D${headersfile} http://localhost:8080/redfish/v1/SessionService/Sessions -X POST -d "{\"UserName\": \"${username}\", \"Password\": \"${password}\"}" 2>&1)
+X_AUTH_TOKEN=$(cat ${headersfile} | grep X-Auth-Token | cut -d: -f2 | perl -p -e 's/\r//g;')
+SESSION_URI=$(cat ${headersfile} | grep Location | cut -d: -f2 | perl -p -e 's/\r//g;')
 
 for i in $X_AUTH_TOKEN
 do
-    X_AUTH_TOKEN=$i
+    export X_AUTH_TOKEN=$i
     break
 done
+
+for i in $SESSION_URI
+do
+    export SESSION_URI=$i
+    break
+done
+
+export AUTH_HEADER="X-Auth-Token: $X_AUTH_TOKEN"
 
 if [ -n "$X_AUTH_TOKEN" ]; then
     echo "export X_AUTH_TOKEN=$X_AUTH_TOKEN"
     echo "export AUTH_HEADER='X-Auth-Token: $X_AUTH_TOKEN'"
+    echo "export SESSION_URI=$SESSION_URI"
 else
     echo "export X_AUTH_TOKEN="
     echo "export AUTH_HEADER="
+    echo "export SESSION_URI="
 fi
+
+rm $headersfile
+trap - EXIT QUIT HUP INT ERR
