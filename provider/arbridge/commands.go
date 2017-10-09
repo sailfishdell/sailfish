47e869799d7e595e212e8b34d9f84df21066bb72
Michael Brown <mebrown@michaels-house.net>
2017-10-09 00:54:44 -0500

    implement setting property

--- provider/arbridge/commands.go
+++ provider/arbridge/commands.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	eh "github.com/looplab/eventhorizon"
 	"github.com/superchalupa/go-redfish/domain"
+	"strings"
 )
 
 const (
@@ -25,6 +26,34 @@ type ProcessAREvent struct {
 
 func (c ProcessAREvent) CommandType() eh.CommandType { return ProcessAREventCommand }
 func (c ProcessAREvent) Handle(ctx context.Context, a *domain.RedfishResourceAggregate) error {
-	fmt.Printf("Running command to process AR Event for aggregate.\n")
+	// walk through all of the properties in this aggregate and see if any have a meta
+	np := make(map[string]interface{})
+
+	// TODO: need to recursively walk the properties. For now, just for testing only do one layer.
+	for k, v := range a.Properties {
+		if strings.HasSuffix(k, "@meta") {
+			// check plugin type
+			if meta, ok := v.(map[string]interface{}); ok {
+				pluginType, ok := meta["plugin"]
+				if ok && pluginType == "AR" {
+					fmt.Printf("\tfound aggregate with matching @meta property: %s\n", k)
+					data, ok := meta["data"]
+					if !ok {
+						continue
+					}
+					if c.Name == data {
+						np[k[:len(k)-5]] = c.Value
+					}
+				}
+			}
+		}
+	}
+
+	a.StoreEvent(domain.RedfishResourcePropertiesUpdatedEvent,
+		&domain.RedfishResourcePropertiesUpdatedData{
+			Properties: np,
+		},
+	)
+
 	return nil
 }
