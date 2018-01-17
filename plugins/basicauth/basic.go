b16b95447c5dfe2183be589cdc066b4e6233f09c
Michael Brown <mebrown@michaels-house.net>
2018-01-17 05:09:10 -0600

    go fmt

--- plugins/basicauth/basic.go
+++ plugins/basicauth/basic.go
@@ -1,30 +1,28 @@
 package basicauth
 
 import (
-    "net/http"
-    "context"
+	"context"
+	"net/http"
 )
-
 
 type AddUserDetails struct {
 	OnUserDetails      func(userName string, privileges []string) http.Handler
 	WithoutUserDetails http.Handler
 }
 
-
 func (a *AddUserDetails) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-    username, password, ok := req.BasicAuth()
-    if ok {
-        privileges := []string{"Unauthenticated", "ConfigureManager", "basicauth"}
-        if username == "root" && password == "password" {
-		    a.OnUserDetails(username, privileges).ServeHTTP(rw, req)
-            return
-        }
-    }
-    a.WithoutUserDetails.ServeHTTP(rw, req)
+	username, password, ok := req.BasicAuth()
+	if ok {
+		privileges := []string{"Unauthenticated", "ConfigureManager", "basicauth"}
+		if username == "root" && password == "password" {
+			a.OnUserDetails(username, privileges).ServeHTTP(rw, req)
+			return
+		}
+	}
+	a.WithoutUserDetails.ServeHTTP(rw, req)
 }
 
 func NewService(ctx context.Context) (aud *AddUserDetails) {
-    aud = &AddUserDetails{}
-    return
+	aud = &AddUserDetails{}
+	return
 }
