e3287094a7a554cc550d1cd4bc15738107e85695
Michael Brown <mebrown@michaels-house.net>
2018-01-20 02:22:21 -0600

    Remove old openssl cert generation

--- scripts/gencerts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# generate a simple self-signed key for SSL
-
-openssl genrsa -out server.key 2048
-openssl ecparam -genkey -name secp384r1 -out server.key
-openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650
