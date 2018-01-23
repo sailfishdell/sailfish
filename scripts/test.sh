ae213a71566d496d30efdd5870675e86fd6f7559
Michael Brown <mebrown@michaels-house.net>
2018-01-23 01:09:26 -0600

    fixup test script to not hardcode url for every line

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-curl http://localhost:8080/api/createresource  -d '
+CURLCMD="curl --cacert ./ca.crt"
+prot=https
+user=root
+pass=password
+host=localhost
+port=8443
+
+URL=$proto://$user:$pass@$host
+
+$CURLCMD http://localhost:8443/api/createresource  -d '
     {
         "ID": "49467bb4-5c1f-473b-af00-5d4fa209f7bf",
         "ResourceURI":"/redfish/v1/test",
@@ -12,12 +21,16 @@ curl http://localhost:8080/api/createresource  -d '
 
 
 echo "/redfish"
-curl http://localhost:8080/redfish
+$CURLCMD $URL/redfish
+
 echo "/redfish/"
-curl http://localhost:8080/redfish/
+$CURLCMD $URL/redfish/
+
 echo "/redfish/v1"
-curl http://localhost:8080/redfish/v1
+$CURLCMD $URL/redfish/v1
+
 echo "/redfish/v1/"
-curl http://localhost:8080/redfish/v1/
+$CURLCMD $URL/redfish/v1/
+
 echo "/redfish/v1/test"
-curl http://root:password@localhost:8080/redfish/v1/test
+$CURLCMD $URL/redfish/v1/test
