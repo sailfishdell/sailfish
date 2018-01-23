fd62b0bdfcc11e9b5114a231a228a2f864555514
Michael Brown <mebrown@michaels-house.net>
2018-01-23 01:15:17 -0600

    fixup path for redfish metadata files to be in v1/ so it doesn't tab complete clash with redfishresource.

--- cmd/ocp-server/main.go
+++ cmd/ocp-server/main.go
@@ -82,8 +82,8 @@ func main() {
 	m.Path("/redfish/v1/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) { http.Redirect(w, r, "/redfish/v1", 301) })
 
 	// some static files that we should generate at some point
-	m.Path("/redfish/v1/$metadata").HandlerFunc(func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "redfish/v1/metadata.xml") })
-	m.Path("/redfish/v1/odata").HandlerFunc(func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "redfish/v1/odata.json") })
+	m.Path("/redfish/v1/$metadata").HandlerFunc(func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "v1/metadata.xml") })
+	m.Path("/redfish/v1/odata").HandlerFunc(func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "v1/odata.json") })
 
 	// generic handler for redfish output on most http verbs
 	// Note: this works by using the session service to get user details from token to pass up the stack using the embedded struct
