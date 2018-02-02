8f5156a8b0dc6ce30bb37c52f69d3501a2c6d1b1
Michael E Brown <michael_e_brown@dell.com>
2018-02-02 17:54:34 -0600

    go fmt

--- plugins/dbus/dbus.go
+++ plugins/dbus/dbus.go
@@ -3,9 +3,9 @@ package dbus
 import (
 	"context"
 	"fmt"
-    "os"
+	"os"
 
-    "github.com/godbus/dbus"
+	"github.com/godbus/dbus"
 	domain "github.com/superchalupa/go-redfish/redfishresource"
 )
 
@@ -14,16 +14,16 @@ var (
 )
 
 func init() {
-    conn, err := dbus.SystemBus()
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Cannot register dbus_property plugin, could not connect to System Bus: %s\n", err.Error())
-        return
-    }
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot register dbus_property plugin, could not connect to System Bus: %s\n", err.Error())
+		return
+	}
 	domain.RegisterPlugin(func() domain.Plugin { return &dbusProperty{conn: conn} })
 }
 
-type dbusProperty struct{
-    conn    *dbus.Conn
+type dbusProperty struct {
+	conn *dbus.Conn
 }
 
 func (t *dbusProperty) PluginType() domain.PluginType { return DbusPropertyPlugin }
@@ -37,22 +37,30 @@ func (t *dbusProperty) DemandBasedUpdate(
 	body interface{},
 ) {
 
-    // morally equivalent to do{}while(0)
-    var ok bool
-    var bus, intfc, path, prop string
-    for once:=true; once; once=false {
-        bus, ok = meta["bus_name"].(string)
-        if !ok { break }
+	// morally equivalent to do{}while(0)
+	var ok bool
+	var bus, intfc, path, prop string
+	for once := true; once; once = false {
+		bus, ok = meta["bus_name"].(string)
+		if !ok {
+			break
+		}
 
-        intfc, ok = meta["interface_name"].(string)
-        if !ok { break }
+		intfc, ok = meta["interface_name"].(string)
+		if !ok {
+			break
+		}
 
-        path, ok = meta["path"].(string)
-        if !ok { break }
+		path, ok = meta["path"].(string)
+		if !ok {
+			break
+		}
 
-        prop, ok = meta["property"].(string)
-        if !ok { break }
-    }
+		prop, ok = meta["property"].(string)
+		if !ok {
+			break
+		}
+	}
 
 	if !ok {
 		fmt.Printf("Misconfigured runcmd plugin, required value not set\n")
@@ -60,11 +68,11 @@ func (t *dbusProperty) DemandBasedUpdate(
 	}
 
 	busObject := t.conn.Object(bus, dbus.ObjectPath(path))
-    variant, err := busObject.GetProperty(intfc + "." + prop)
-    if err != nil {
+	variant, err := busObject.GetProperty(intfc + "." + prop)
+	if err != nil {
 		fmt.Printf("Error getting property: %s\n", err.Error())
-        return
-    }
+		return
+	}
 
 	rrp.Value = variant.Value()
 }
