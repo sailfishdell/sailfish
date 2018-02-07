020d0b79fee9fd7a246ac6e55e1ded1402d1c21c
Michael Brown <mebrown@michaels-house.net>
2018-02-07 08:45:58 -0600

    Add static handler for serving local copies of schemas

--- cmd/ocp-server/main.go
+++ cmd/ocp-server/main.go
@@ -144,6 +144,9 @@ func main() {
 	m.Path("/redfish/v1/$metadata").HandlerFunc(func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "v1/metadata.xml") })
 	m.Path("/redfish/v1/odata").HandlerFunc(func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "v1/odata.json") })
 
+    // serve up the schema XML
+    m.PathPrefix("/schemas/v1/").Handler(http.StripPrefix("/schemas/v1/", http.FileServer(http.Dir("./schemas/v1/"))) )
+
 	// generic handler for redfish output on most http verbs
 	// Note: this works by using the session service to get user details from token to pass up the stack using the embedded struct
 	m.PathPrefix("/redfish/v1").Methods("GET", "PUT", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS").Handler(sessionServiceAuthorizer)
