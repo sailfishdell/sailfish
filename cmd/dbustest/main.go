139c8ac36cd3e4354504f95d84b80a08652f0882
michael_e_brown <michael_e_brown@dell.com>
2018-02-10 10:58:50 -0600

    Start adding mapper support

--- cmd/dbustest/main.go
+++ cmd/dbustest/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/godbus/dbus"
@@ -21,20 +22,33 @@ var Path dbus.ObjectPath = "/xyz/openbmc_project/software/14880bfa"
 var Properties []string = []string{"Purpose", "Version"}
 
 func main() {
+	ctx := context.Background()
 	conn, err := dbus.SystemBus()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to connect to system bus:", err)
 		os.Exit(1)
 	}
 
-	busObject := conn.Object(BusName, Path)
-
-	for _, p := range Properties {
-		variant, err := busObject.GetProperty(Interface + "." + p)
-		if err != nil {
-			log.Fatalln("Error getting property:", err)
-			continue
-		}
-		log.Println("Variant --->", variant.String())
+	m := New(conn)
+	ret, err := m.GetSubTreePaths(ctx, "/xyz/openbmc_project", 0, "xyz.openbmc_project.Sensor.Value")
+	if err == nil {
+		b, _ := json.MarshalIndent(ret, "", "  ")
+		fmt.Printf("DBUS CALL RETURN: %s\n", b)
+	} else {
+		fmt.Printf("Error from dbus call: %s\n", err.Error())
 	}
+
+	//m.GetSubTree(ctx,  "/xyz/openbmc_project", 0)
+
+	/*
+		busObject := conn.Object(BusName, Path)
+		for _, p := range Properties {
+			variant, err := busObject.GetProperty(Interface + "." + p)
+			if err != nil {
+				log.Fatalln("Error getting property:", err)
+				continue
+			}
+			log.Println("Variant --->", variant.String())
+		}
+	*/
 }
---
+++ cmd/dbustest/mapper.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/godbus/dbus"
+)
+
+type mapper struct {
+	conn         *dbus.Conn
+	mapperObject dbus.BusObject
+}
+
+const (
+	MapperBusName    = "xyz.openbmc_project.ObjectMapper"
+	MapperObjectPath = "/xyz/openbmc_project/object_mapper"
+	MapperInterface  = "xyz.openbmc_project.ObjectMapper"
+
+	getSubTreePaths string = MapperInterface + ".GetSubTreePaths"
+	getSubTree      string = MapperInterface + ".GetSubTree"
+
+	DbusTimeout = 10
+)
+
+func New(conn *dbus.Conn) *mapper {
+	mo := conn.Object(MapperBusName, MapperObjectPath)
+
+	return &mapper{
+		conn:         conn,
+		mapperObject: mo,
+	}
+}
+
+func (m *mapper) GetAncestors() {}
+func (m *mapper) GetObject()    {}
+
+func (m *mapper) GetSubTreePaths(ctx context.Context, path string, depth int, interfaces ...string) ([]interface{}, error) {
+	// sias : path, depth (0-unlimited), array of interfaces
+	timedctx, cancel := context.WithTimeout(ctx, time.Duration(DbusTimeout))
+	defer cancel()
+	call, err := m.DbusCall(timedctx, dbus.Flags(0), getSubTreePaths, path, depth, interfaces)
+	return call.Body, err
+}
+
+func (m *mapper) GetSubTree(ctx context.Context, path string, depth int, interfaces ...string) ([]interface{}, error) {
+	// sias : path, depth (0-unlimited), array of interfaces
+	timedctx, cancel := context.WithTimeout(ctx, time.Duration(DbusTimeout))
+	defer cancel()
+	call, err := m.DbusCall(timedctx, dbus.Flags(0), getSubTree, path, depth, interfaces)
+	return call.Body, err
+}
+
+func (m *mapper) DbusCall(ctx context.Context, flags dbus.Flags, fn string, args ...interface{}) (*dbus.Call, error) {
+
+	callObj := m.mapperObject.Call(fn, flags, args...)
+	if callObj.Err != nil {
+		fmt.Printf("Pre-error: %s\n", callObj.Err.Error())
+		return callObj, callObj.Err
+	}
+
+	select {
+	case <-callObj.Done:
+		fmt.Printf("donecall. err: %s return: %s\n", callObj.Err.Error(), callObj.Body)
+	case <-ctx.Done():
+		// give up (use with context.WithTimeout() or context.WithDeadline() to implement a timeout)
+	}
+
+	return callObj, callObj.Err
+}
