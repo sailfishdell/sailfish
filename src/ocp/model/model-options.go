5f268044309b7e2394b3f43a32fa408b83dff372
michael_e_brown <michael_e_brown@dell.com>
2018-06-05 22:59:22 -0500

    Remove unused model options now that we've migrated these to the views.

--- src/ocp/model/model-options.go
+++ src/ocp/model/model-options.go
@@ -1,15 +1,10 @@
 package model
 
-import (
-	eh "github.com/looplab/eventhorizon"
-	domain "github.com/superchalupa/go-redfish/src/redfishresource"
-)
+import ()
 
-//
 //  Options: these are construction-time functional options that can be passed
 //  to the constructor, or after construction, you can pass them with
 //  ApplyOptions
-//
 
 // UpdateProperty is a functional option to set an option at construction time or update the value after using ApplyOption.
 // Model is locked for Options in ApplyOption
@@ -20,7 +15,7 @@ func UpdateProperty(p string, v interface{}) Option {
 	}
 }
 
-// Model is locked for Options in ApplyOption
+// PropertyOnce is used to set a property, will fail (panic) if somebody else has already set it
 func PropertyOnce(p string, v interface{}) Option {
 	return func(s *Model) error {
 		if _, ok := s.properties[p]; ok {
@@ -29,16 +24,4 @@ func PropertyOnce(p string, v interface{}) Option {
 		s.properties[p] = v
 		return nil
 	}
-}
-
-func URI(uri string) Option {
-	return UpdateProperty("uri", uri)
-}
-
-func UUID() Option {
-	return UpdateProperty("id", eh.NewUUID())
-}
-
-func PluginType(pt domain.PluginType) Option {
-	return UpdateProperty("plugin_type", pt)
 }
