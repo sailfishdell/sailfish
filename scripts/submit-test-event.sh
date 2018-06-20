058b09ce7f43395fab91f140ec6b7ade35f61c7d
michael_e_brown <michael_e_brown@dell.com>
2018-06-19 23:19:45 -0500

    bugfix for save-events script

---
+++ scripts/submit-test-event.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ -d '{ "Action": "SubmitTestEvent", "EventType": "StatusChange", "EventID": "<string>", "EventTimestamp": "<date-time>", "Severity": "OK", "Message": "<string>", "MessageID": "<string>", "MessageArgs": [<"string">], "OriginOfCondition": "<string>"}'
+
