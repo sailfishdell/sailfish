a063400a05c0fbdb32fd159f74eca8e846fa1306
michael_e_brown <michael_e_brown@dell.com>
2018-06-25 22:35:59 -0500

    Fixup test script to output correct event schema

--- scripts/test-submit-event.sh
+++ scripts/test-submit-event.sh
@@ -9,5 +9,5 @@ curl  --noproxy '*' -X POST http://Administrator:password@localhost:8080/redfish
     "Message": "The LAN has been disconnected",
     "MessageId": "Alert.1.0.LanDisconnect",
     "MessageArgs": [ "EthernetInterface 1", "/redfish/v1/Systems/1" ],
-    "OriginOfCondition": "/redfish/v1/Systems/1/EthernetInterfaces/1"
+    "OriginOfCondition": { "@odata.id": "/redfish/v1/Systems/1/EthernetInterfaces/1" }
 }' 
