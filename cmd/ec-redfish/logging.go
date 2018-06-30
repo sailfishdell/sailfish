48c4b0aeda802efe61c52b7589447ab409adbea3
michael_e_brown <michael_e_brown@dell.com>
2018-06-30 17:40:27 -0500

    More docs for log config Ability to enable/disable individual log destinations

--- cmd/ec-redfish/logging.go
+++ cmd/ec-redfish/logging.go
@@ -96,6 +96,7 @@ func (l *MyLogger) Notify(ctx context.Context, event eh.Event) {
 }
 
 type LoggingConfig struct {
+    Enabled         bool
 	Level           string
 	FileName        string
 	EnableStderr    bool
@@ -115,6 +116,10 @@ func (l *MyLogger) setupLogHandlersFromConfig(cfg *viper.Viper) {
 
 	topLevelHandlers := []log.Handler{}
 	for _, logcfg := range LogConfig {
+        if ! logcfg.Enabled {
+            continue
+        }
+
 		var outputHandler log.Handler
 		switch path := logcfg.FileName; path {
 		case "":
