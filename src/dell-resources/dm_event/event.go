e234f1ae43f7929487a0e52696d1d610776470eb
josh_pennell <josh_pennell@dell.com>
2018-07-05 23:12:57 +0000

    JIT-0000 adding DM fan event
    Change-Id: Ia44669ae5250d4c0f3f03c30a47b44efa750a0d4

--- src/dell-resources/dm_event/event.go
+++ src/dell-resources/dm_event/event.go
@@ -7,10 +7,12 @@ import (
 const (
 	HealthEvent      = eh.EventType("HealthEvent")
 	DataManagerEvent = eh.EventType("DataManagerEvent")
+	FanEvent         = eh.EventType("FanEvent")
 )
 
 func init() {
 	eh.RegisterEventData(HealthEvent, func() eh.EventData { return &HealthEventData{} })
+	eh.RegisterEventData(FanEvent, func() eh.EventData { return &FanEventData{} })
 	eh.RegisterEventData(DataManagerEvent, func() eh.EventData {
 		var f DataManagerEventData
 		return f
@@ -20,6 +22,34 @@ func init() {
 type HealthEventData struct {
 	FQDD   string
 	Health string
+}
+
+type DataObjectHeader struct {
+	ObjStatus       int `json:"objStatus"`
+	ObjSize         int `json:"objSize"`
+	ObjType         int `json:"objType"`
+	RefreshInterval int `json:"refreshInterval"`
+	ObjFlags        int `json:"objFlags"`
+	FQDD            string
+	Struct          string
+}
+
+type FanEventData struct {
+	ObjectHeader      DataObjectHeader
+	Fanpwm            float64 `json:"fanpwm"`
+	Key               string
+	FanName           string
+	Fanpwm_int        int `json:"fanpwm_int"`
+	VendorName        string
+	WarningThreshold  int `json:"warningThreshold"`
+	DeviceName        string
+	TachName          string
+	CriticalThreshold int `json:"criticalThreshold"`
+	Fanhealth         int `json:"fanhealth"`
+	Numrotors         int `json:"numrotors"`
+	Rotor2rpm         int `json:"rotor2rpm"`
+	Rotor1rpm         int `json:"rotor1rpm"`
+	FanStateMask      int `json:"fanStateMask"`
 }
 
 type DataManagerEventData interface{}
--- src/dell-resources/fan_controller/controller.go
+++ src/dell-resources/fan_controller/controller.go
@@ -34,87 +34,32 @@ func Setup(ctx context.Context, ch eh.CommandHandler, eb eh.EventBus) {
 }
 
 func new(ctx context.Context, logger log.Logger, m *model.Model, fqdd string, ch eh.CommandHandler, eb eh.EventBus, ew waiter) error {
+
 	// stream processor for action events
-	sp, err := event.NewEventStreamProcessor(ctx, ew, event.CustomFilter(selectDataManagerEvent()))
+	sp, err := event.NewEventStreamProcessor(ctx, ew, event.CustomFilter(selectFanEvent()))
 	if err != nil {
 		logger.Error("Failed to create event stream processor", "err", err)
 		return err
 	}
 	sp.RunForever(func(event eh.Event) {
-		if data, ok := event.Data().(dm_event.DataManagerEventData); ok {
-			datamap, ok := data.(map[string]interface{})
-			if !ok {
-				logger.Warn("Data wasn't a map, odd..", "data", data)
-				return
+		if data, ok := event.Data().(*dm_event.FanEventData); ok {
+			logger.Debug("Process Event", "data", data)
+			if data.ObjectHeader.FQDD == fqdd {
+				m.UpdateProperty("Fanwpm", data.Fanpwm)
+				m.UpdateProperty("Key", data.Key)
+				m.UpdateProperty("FanName", data.FanName)
+				m.UpdateProperty("Fanpwm_int", data.Fanpwm_int)
+				m.UpdateProperty("VendorName", data.VendorName)
+				m.UpdateProperty("WarningThreshold", data.WarningThreshold)
+				m.UpdateProperty("DeviceName", data.DeviceName)
+				m.UpdateProperty("TachName", data.TachName)
+				m.UpdateProperty("CriticalThreshold", data.CriticalThreshold)
+				m.UpdateProperty("Fanhealth", data.Fanhealth)
+				m.UpdateProperty("Numrotors", data.Numrotors)
+				m.UpdateProperty("Rotor2rpm", data.Rotor2rpm)
+				m.UpdateProperty("Rotor1rpm", data.Rotor1rpm)
+				m.UpdateProperty("FanStateMask", data.FanStateMask)
 			}
-
-			eventFqdd, ok := datamap["FQDD"]
-			if !ok {
-				// logger.Warn("Data map didn't have an FQDD, that's odd...", "data", data)
-				return
-			}
-
-			if eventFqdd != fqdd {
-				logger.Debug("This is not the event we are looking for.", "data", data)
-				return
-			}
-
-			logger.Warn("Fan controller: Process Event", "data", data)
-
-			thp, ok := datamap["thp_fan_data_object"]
-			if !ok {
-				return
-			}
-
-			dataobj := thp.(map[string]interface{})
-			if ok {
-				m.StopNotifications()
-				if fanhealth, ok := dataobj["fanhealth"]; ok {
-					m.UpdateProperty("dm_fanhealth", fanhealth)
-				}
-				if fanpwm, ok := dataobj["fanpwm"]; ok {
-					m.UpdateProperty("fanpwm", fanpwm)
-				}
-				if numrotors, ok := dataobj["numrotors"]; ok {
-					m.UpdateProperty("numrotors", numrotors)
-				}
-				if rotor1rpm, ok := dataobj["rotor1rpm"]; ok {
-					m.UpdateProperty("rotor1rpm", rotor1rpm)
-				}
-				if rotor2rpm, ok := dataobj["rotor2rpm"]; ok {
-					m.UpdateProperty("rotor2rpm", rotor2rpm)
-				}
-				if warningThreshold, ok := dataobj["warningThreshold"]; ok {
-					m.UpdateProperty("warningThreshold", warningThreshold)
-				}
-				if criticalThreshold, ok := dataobj["criticalThreshold"]; ok {
-					m.UpdateProperty("criticalThreshold", criticalThreshold)
-				}
-				m.StartNotifications()
-				m.NotifyObservers()
-			}
-
-			/*
-			   "map[
-			       FQDD:System.Chassis.1#Fan.Slot.1
-			       objFlags:8
-			       objSize:138
-			       objStatus:2
-			       objType:3330
-			       refreshInterval:0
-			       thp_fan_data_object:map[
-			           warningThreshold:1.936941416e+09
-			           fanhealth:7
-			           objExtFlags:108
-			           rotor1rpm:1
-			           rotor2rpm:1.953724755e+09
-			           numrotors:0
-			           criticalThreshold:1.127116133e+09
-			           fanStateMask:8.25127785e+08
-			           fanpwm:2.8992865226880465e-42
-			           fanpwm_int:0]]
-			*/
-
 		} else {
 			logger.Warn("Should never happen: got an invalid event in the event handler")
 		}
@@ -123,9 +68,9 @@ func new(ctx context.Context, logger log.Logger, m *model.Model, fqdd string, ch
 	return nil
 }
 
-func selectDataManagerEvent() func(eh.Event) bool {
+func selectFanEvent() func(eh.Event) bool {
 	return func(event eh.Event) bool {
-		if event.EventType() == dm_event.DataManagerEvent {
+		if event.EventType() == dm_event.FanEvent {
 			return true
 		}
 		return false
