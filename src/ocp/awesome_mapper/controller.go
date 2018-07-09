731728dfdb0ffac7860e4c72a678caa27b0137a2
michael_e_brown <michael_e_brown@dell.com>
2018-07-09 00:22:39 -0500

    Finish up awesome mapper, fully works now for basic cases that can be tested.

--- src/ocp/awesome_mapper/controller.go
+++ src/ocp/awesome_mapper/controller.go
@@ -1,11 +1,11 @@
 package awesome_mapper
 
 import (
-	"context"
-	"errors"
+    "context"
+    "errors"
 
-	"github.com/Knetic/govaluate"
 	"github.com/spf13/viper"
+    "github.com/Knetic/govaluate"
 
 	eh "github.com/looplab/eventhorizon"
 
@@ -14,17 +14,22 @@ import (
 	"github.com/superchalupa/go-redfish/src/ocp/model"
 )
 
+type Evaluable interface {
+    Evaluate(map[string]interface{}) (interface{}, error)
+}
+
 type mapping struct {
-	Property string
-	Query    string
+	Property   string
+	Query string
+    expr  Evaluable
 }
 
 type MappingEntry struct {
-	Select      string
-	ModelUpdate []mapping
+    Select      string
+    ModelUpdate []*mapping
 }
 
-func New(ctx context.Context, logger log.Logger, cfg *viper.Viper, m *model.Model, name string, parameters map[string]interface{}) error {
+func New(ctx context.Context, logger log.Logger, cfg *viper.Viper, mdl *model.Model, name string, parameters map[string]interface{}) (error) {
 	c := []MappingEntry{}
 
 	k := cfg.Sub("mappings")
@@ -38,17 +43,42 @@ func New(ctx context.Context, logger log.Logger, cfg *viper.Viper, m *model.Mode
 	}
 	logger.Warn("updated mappings", "mappings", c)
 
-	try := c[0]
+    functions := map[string]govaluate.ExpressionFunction{}
 
-	// stream processor for action events
-	sp, err := event.NewESP(ctx, event.ExpressionFilter(logger, try.Select, parameters, map[string]govaluate.ExpressionFunction{}))
-	if err != nil {
-		logger.Error("Failed to create event stream processor", "err", err, "select-string", try.Select)
-		return err
-	}
-	sp.RunForever(func(event eh.Event) {
-		logger.Crit("GOT AN EVENT", "event", event)
-	})
+outer:
+    for _, entry := range c {
+        loopvar := entry
+        for _, query := range loopvar.ModelUpdate {
+            query.expr, err = govaluate.NewEvaluableExpressionWithFunctions(query.Query, functions)
+            if err != nil {
+                logger.Crit("Query construction failed", "query", query.Query)
+                continue  outer
+            }
+        }
+
+        // stream processor for action events
+        sp, err := event.NewESP(ctx, event.ExpressionFilter(logger, loopvar.Select, parameters, functions))
+        if err != nil {
+            logger.Error("Failed to create event stream processor", "err", err, "select-string", loopvar.Select)
+            continue
+        }
+
+        sp.RunForever(func(event eh.Event) {
+            logger.Crit("GOT AN EVENT", "event", event)
+            for _, query := range loopvar.ModelUpdate {
+                if query.expr == nil {
+                    logger.Error("query is nil", "loopvar", loopvar)
+                    continue
+                }
+                val, err := query.expr.Evaluate(parameters)
+                if err != nil {
+                    logger.Error("Expression failed to evaluate", "query.expr", query.expr, "parameters", parameters)
+                    continue
+                }
+                mdl.UpdateProperty(query.Property, val)
+            }
+        })
+    }
 
 	return nil
 }
