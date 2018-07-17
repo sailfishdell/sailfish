f71589b574ad9ba36013ef2538a2e24099f92afd
michael_e_brown <michael_e_brown@dell.com>
2018-07-17 09:21:03 -0500

    Have vegeta log all output for posterity as well as add support to get 'top' data for cpu profiling

--- scripts/vegeta-test.sh
+++ scripts/vegeta-test.sh
@@ -10,23 +10,40 @@ scriptdir=$(cd $(dirname $0); pwd)
 
 outputdir=${1:-out/}
 skiplist=${2:-}
+runtoken=${runtoken:-1}
+runbasic=${runbasic:-0}
 
+cleanup() {
+    # close FDs to ensure tee finishes
+    exec 1>&0 2>&1
+    if [ -n "$TEEPID" ];then
+        while ps --pid $TEEPID > /dev/null 2>&1
+        do
+            sleep 1
+        done
+    fi
+}
+trap 'cleanup' EXIT
 
 if [ ! -e $outputdir/to-visit.txt ]; then
     tempdir=$(mktemp -d ./output-XXXXXX)
-    trap 'rm -rf $tempdir' EXIT
+    trap 'cleanup; [ -n "$tepdir" ] && rm -rf $tempdir' EXIT
 
     . $scriptdir/walk.sh $tempdir
     outputdir=${1:-out/}
 
     mkdir -p $outputdir/
-    cp $tempdir/to-visit.txt $outputdir/to-visit.txt
+    cat $tempdir/errors.txt $tempdir/to-visit.txt | sort | uniq -u | grep -v /redfish/v1/SessionService/Sessions/  > $outputdir/to-visit.txt  ||:
 
     rm -rf $tempdir
 fi
 
 rm -rf $outputdir/{token,basic}
 mkdir -p $outputdir $outputdir/token $outputdir/basic
+LOGFILE=$outputdir/script-output.txt
+exec 1> >(exec -a 'LOGGING TEE' tee $LOGFILE) 2>&1
+TEEPID=$!
+
 cat $outputdir/to-visit.txt | perl -p -i -e "s|^|GET ${BASE}|" > $outputdir/vegeta-targets.txt
 cat $outputdir/to-visit.txt | perl -p -i -e "s|^|GET ${prot}://${user}:${pass}\@${host}:${port}|" > $outputdir/basic/vegeta-targets.txt
 
@@ -42,20 +59,36 @@ echo "Running vegeta"
 
 time=10s
 
+savetop() {
+    echo "Starting TOP for vegeta run. RATE: $index" > $1
+    ssh root@${host} 'top -b -d1 -o %MEM' >> $1 &
+    SSHPID=$!
+}
+
 for i in $(seq 30 ) $(seq 40 10 100) ; do
     index=$(printf "%03d" $i)
-    vegeta attack -targets $outputdir/vegeta-targets.txt -output $outputdir/token/results-rate-${index}.bin -header "$AUTH_HEADER" -duration=${time} $cert_opt -rate $i
 
-    cat $outputdir/token/results-rate-${index}.bin | vegeta report -reporter text > $outputdir/token/report-r${index}-text.txt
-    cat $outputdir/token/results-rate-${index}.bin | vegeta report -reporter plot > $outputdir/token/report-r${index}-plot.html
-    cat $outputdir/token/results-rate-${index}.bin | vegeta report -reporter='hist[0,2ms,4ms,6ms,8ms,10ms,20ms,30ms,40ms,60ms,80ms,100ms,200ms,400ms,800ms,1600ms,3200ms,6400ms]' > $outputdir/token/report-r${index}-hist.txt
+    if [ ${runtoken} == 1 ]; then
+        [ "${profile}" == 1 ] && savetop $outputdir/token/results-r${index}-CPU.txt
+        vegeta attack -targets $outputdir/vegeta-targets.txt -output $outputdir/token/results-rate-${index}.bin -header "$AUTH_HEADER" -duration=${time} $cert_opt -rate $i
+        [ -n "$SSHPID" ] && kill $SSHPID ||:
 
-    vegeta attack -targets $outputdir/basic/vegeta-targets.txt -output $outputdir/basic/results-rate-${index}.bin -duration=${time} $cert_opt -rate $i
-    cat $outputdir/basic/results-rate-${index}.bin | vegeta report -reporter text > $outputdir/basic/report-r${index}-text.txt
-    cat $outputdir/basic/results-rate-${index}.bin | vegeta report -reporter plot > $outputdir/basic/report-r${index}-plot.html
-    cat $outputdir/basic/results-rate-${index}.bin | vegeta report -reporter='hist[0,2ms,4ms,6ms,8ms,10ms,20ms,30ms,40ms,60ms,80ms,100ms,200ms,400ms,800ms,1600ms,3200ms,6400ms]' > $outputdir/basic/report-r${index}-hist.txt
+        cat $outputdir/token/results-rate-${index}.bin | vegeta report -reporter text > $outputdir/token/report-r${index}-text.txt
+        cat $outputdir/token/results-rate-${index}.bin | vegeta report -reporter plot > $outputdir/token/report-r${index}-plot.html
+        cat $outputdir/token/results-rate-${index}.bin | vegeta report -reporter='hist[0,2ms,4ms,6ms,8ms,10ms,20ms,30ms,40ms,60ms,80ms,100ms,200ms,400ms,800ms,1600ms,3200ms,6400ms]' > $outputdir/token/report-r${index}-hist.txt
+    fi
 
-    cat  $outputdir/token/report-r${index}-text.txt  $outputdir/basic/report-r${index}-text.txt
+    if [ ${runbasic} == 1 ]; then
+        [ "${profile}" == 1 ] && savetop $outputdir/basic/results-r${index}-CPU.txt
+        vegeta attack -targets $outputdir/basic/vegeta-targets.txt -output $outputdir/basic/results-rate-${index}.bin -duration=${time} $cert_opt -rate $i
+        [ -n "$SSHPID" ] && kill $SSHPID ||:
+
+        cat $outputdir/basic/results-rate-${index}.bin | vegeta report -reporter text > $outputdir/basic/report-r${index}-text.txt
+        cat $outputdir/basic/results-rate-${index}.bin | vegeta report -reporter plot > $outputdir/basic/report-r${index}-plot.html
+        cat $outputdir/basic/results-rate-${index}.bin | vegeta report -reporter='hist[0,2ms,4ms,6ms,8ms,10ms,20ms,30ms,40ms,60ms,80ms,100ms,200ms,400ms,800ms,1600ms,3200ms,6400ms]' > $outputdir/basic/report-r${index}-hist.txt
+    fi
+
+    cat  $outputdir/token/report-r${index}-text.txt  $outputdir/basic/report-r${index}-text.txt ||:
 done
 
 
