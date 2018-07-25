a5b059345eb959cceade9a4596bf9c9a345bdc00
michael_e_brown <michael_e_brown@dell.com>
2018-07-25 16:50:42 -0500

    Update idrac benchmark script to also start trying to test go

--- scripts/benchmark-IDRAC.sh
+++ scripts/benchmark-IDRAC.sh
@@ -22,20 +22,18 @@ if [ -z "${GORFHOST}" -a -z "${IDRACHOST}" ]; then
     exit 1
 fi
 
-# FOR EC ODATALITE TESTING ONLY: set TOKEN to 'oauthtest token' output and this script will set and unset as appropriate to benchark each stack
-
 export CURL_OPTS=-k
 export prot=https
 export sqliteuri=${sqliteuri:-/redfish/v1/Chassis/System.Embedded.1/PCIeDevice/3-0}
 
 # run TOP and save results during each run
-export profile=1
+export profile=${profile:-1}
 
 # run basic auth tests for each that supports
-export runbasic=1
+export runbasic=${runbasic:-1}
 
 # run token auth tests for each that supports
-export runtoken=1
+export runtoken=${runtoken:-1}
 mkdir bench ||:
 
 ####################
@@ -47,7 +45,7 @@ export pass=calvin
 export uri=/redfish/v1/Managers/iDRAC.Embedded.1
 host=$IDRACHOST port=443 ${scriptdir}/walk.sh        ${out}/odatalite-walk
 host=$IDRACHOST port=443 ${scriptdir}/runab.sh       ${out}/odatalite-OLD-ab
-host=$IDRACHOST port=443 ${scriptdir}/vegeta-test.sh ${out}/odatalite-vegeta
+host=$IDRACHOST port=443 ${scriptdir}/vegeta-test.sh ${out}/odatalite-ALL-vegeta
 
 # this uri is using the new sqlite
 uri=${sqliteuri}    \
@@ -55,5 +53,26 @@ host=$IDRACHOST port=443 ${scriptdir}/runab.sh       ${out}/odatalite-NEW-ab
 
 # select out the new sqlite URIs for specific bench by vegeta
 mkdir ${out}/odatalite-NEW-vegeta ||:
-grep PCIeDevice ${out}/odatalite-vegeta/to-visit.txt > ${out}/odatalite-NEW-vegeta/to-visit.txt
+grep PCIeDevice ${out}/odatalite-ALL-vegeta/to-visit.txt > ${out}/odatalite-NEW-vegeta/to-visit.txt
 host=$IDRACHOST port=443 ${scriptdir}/vegeta-test.sh ${out}/odatalite-NEW-vegeta
+
+
+####################
+# go-redfish tests
+####################
+export user=Administrator
+export pass=password
+host=$IDRACHOST TOKEN= port=8443 ${scriptdir}/walk.sh        ${out}/go-https-walk
+host=$IDRACHOST TOKEN= port=8443 ${scriptdir}/vegeta-test.sh ${out}/go-https-vegeta
+host=$IDRACHOST TOKEN= port=8443 ${scriptdir}/runhey.sh      ${out}/go-https-hey
+host=$IDRACHOST TOKEN= port=8443 ${scriptdir}/runab.sh       ${out}/go-https-ab
+
+
+# test go-redfish through apache
+mkdir -p ${out}/go-apache-vegeta
+grep /Attributes ${out}/go-https-vegeta/to-visit.txt > ${out}/go-apache-vegeta/to-visit.txt
+host=$IDRACHOST port=2443 ${scriptdir}/vegeta-test.sh     ${out}/go-apache-vegeta
+host=$IDRACHOST port=2443 ${scriptdir}/runhey.sh          ${out}/go-apache-hey
+host=$IDRACHOST port=2443 ${scriptdir}/runab.sh           ${out}/go-apache-ab
+
+
