a5e38d2755217f6e62d4c1e6cb5236a528f25113
michael_e_brown <michael_e_brown@dell.com>
2018-07-24 23:01:07 -0500

    Remove unused logging

--- src/redfishresource/redfish_handler.go
+++ src/redfishresource/redfish_handler.go
@@ -70,13 +70,6 @@ func (rh *RedfishHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	cmdID := eh.NewUUID()
 	reqCtx := WithRequestID(r.Context(), cmdID)
 
-	/*
-		rh.logger.Debug("In ServeHTTP")
-		for name, headers := range r.Header {
-			rh.logger.Debug("HTTP Header", "name", name, "headers", headers)
-		}
-	*/
-
 	// All operations have to be on URLs that exist, so look it up in the tree
 	aggID, ok := rh.d.GetAggregateIDOK(r.URL.Path)
 	if !ok {
