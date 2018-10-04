244b1734d7f1aadc8d217f1ae60f024e657777b6
henry_pang <henry_pang@dell.com>
2018-10-04 16:57:26 +0000

    NGMECNET-496 added csr file check
    Change-Id: I09146b106e99a9c96517dbfcc0eabb96e7735f74

--- src/dell-resources/certificateservices/aggregate.go
+++ src/dell-resources/certificateservices/aggregate.go
@@ -2,6 +2,7 @@ package certificateservices
 
 import (
 	"context"
+  "io/ioutil"
 
 	"github.com/superchalupa/sailfish/src/ocp/view"
 	domain "github.com/superchalupa/sailfish/src/redfishresource"
@@ -9,8 +10,33 @@ import (
 	eh "github.com/looplab/eventhorizon"
 )
 
+func ReadFileContent (string) (f string) {
+  b, err := ioutil.ReadFile(f)
+  if err != nil {
+    b = nil
+  }
+  return string(b)
+}
+
 func AddAggregate(ctx context.Context, v *view.View, baseUri string, ch eh.CommandHandler) (ret eh.UUID) {
 	ret = eh.NewUUID()
+
+  properties := map[string]interface{}{
+    "Id":                             "CertificateService",
+    "Name":                           "Certificate Service",
+    "Description":                    "Represents the properties of Certificate Service",
+    "CertificateSigningRequest@meta": v.Meta(view.PropGET("certificate_signing_request")),
+    "Actions": map[string]interface{}{
+      "#DellCertificateService.GenerateCSR": map[string]interface{}{
+        "target": v.GetActionURI("certificates.generatecsr"),
+      },
+    },
+    "CertificateInventory": map[string]interface{}{
+      "@odata.id": v.GetURI() + "CertificateService/CertificateInventory",
+    },
+  }
+
+  properties["CertificateSigningRequest"] = ReadFileContent("/var/run/FACT_CERT/fact_csr.csr")
 
 	ch.HandleCommand(
 		ctx,
@@ -27,20 +53,8 @@ func AddAggregate(ctx context.Context, v *view.View, baseUri string, ch eh.Comma
 				"PATCH":  []string{},
 				"DELETE": []string{},
 			},
-			Properties: map[string]interface{}{
-				"Id":                             "CertificateService",
-				"Name":                           "Certificate Service",
-				"Description":                    "Represents the properties of Certificate Service",
-				"CertificateSigningRequest@meta": v.Meta(view.PropGET("certificate_signing_request")), //TODO
-				"Actions": map[string]interface{}{
-					"#DellCertificateService.GenerateCSR": map[string]interface{}{
-						"target": v.GetActionURI("certificates.generatecsr"),
-					},
-				},
-				"CertificateInventory": map[string]interface{}{
-					"@odata.id": v.GetURI() + "CertificateService/CertificateInventory",
-				},
-			}})
+			Properties: properties,
+		})
 
 	ch.HandleCommand(
 		ctx,
