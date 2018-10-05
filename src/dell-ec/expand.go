8f4247e5d3abd1be3f6e5db75133b50044f2e4b8
michael_e_brown <michael_e_brown@dell.com>
2018-10-05 10:22:16 -0500

    Add expand formatter to expand one item

--- src/dell-ec/expand.go
+++ src/dell-ec/expand.go
@@ -50,6 +50,39 @@ func makeExpandFormatter(d *domain.DomainObjects) func(context.Context, *view.Vi
 	}
 }
 
+func makeExpandOneFormatter(d *domain.DomainObjects) func(context.Context, *view.View, *model.Model, *domain.RedfishResourceProperty, map[string]interface{}) error {
+	return func(
+		ctx context.Context,
+		v *view.View,
+		m *model.Model,
+		rrp *domain.RedfishResourceProperty,
+		meta map[string]interface{},
+	) error {
+		p, ok := meta["property"].(string)
+
+		uri, ok := m.GetProperty(p).(string)
+		if !ok {
+			return errors.New("uri property not setup properly")
+		}
+
+		aggID, ok := d.GetAggregateIDOK(uri)
+		if !ok {
+			return errors.New("could not find aggregate")
+		}
+		agg, _ := d.AggregateStore.Load(ctx, domain.AggregateType, aggID)
+		redfishResource, ok := agg.(*domain.RedfishResourceAggregate)
+		if !ok {
+			return errors.New("specified uri wasn't an aggregate (impossible!)")
+		}
+
+		redfishResource.PropertiesMu.RLock()
+		rrp.Value, _ = domain.ProcessGET(ctx, redfishResource.Properties)
+		redfishResource.PropertiesMu.RUnlock()
+
+		return nil
+	}
+}
+
 func countFormatter(
 	ctx context.Context,
 	v *view.View,
