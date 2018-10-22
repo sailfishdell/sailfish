774856adc06cb06a74d6cdbd82645e3c8d0360af
Michael Brown <mebrown@michaels-house.net>
2018-10-21 23:35:36 -0500

    fixup to final log levels for slots

--- src/dell-resources/slots/entrypoint.go
+++ src/dell-resources/slots/entrypoint.go
@@ -3,7 +3,6 @@ package slots
 import (
 	"context"
 	"errors"
-	"fmt"
 	"strings"
 	"sync"
 
@@ -20,6 +19,7 @@ type viewer interface {
 
 // StartService will create a model, view, and controller for the eventservice, then start a goroutine to publish events
 func CreateSlotCollection(ctx context.Context, baseView viewer, cfgMgr *viper.Viper, instantiateSvc *testaggregate.Service, modParams func(map[string]interface{}) map[string]interface{}) {
+	var slotLogger log.Logger
 	myModParams := func(in ...map[string]interface{}) map[string]interface{} {
 		mod := map[string]interface{}{}
 		mod["collection_uri"] = baseView.GetURI() + "/Slots"
@@ -30,22 +30,21 @@ func CreateSlotCollection(ctx context.Context, baseView viewer, cfgMgr *viper.Vi
 		}
 		return modParams(mod)
 	}
-	awesome_mapper2.AddFunction("echo", func(args ...interface{}) (interface{}, error) {
-		fmt.Println(args...)
-		return true, nil
-	})
 
-	// create hash to keep track of the slots we instantiate
+	// this is used in the closure below.
 	slotsMu := sync.Mutex{}
+	// create hash to keep track of the slots we instantiate
 	slots := map[string]struct{}{}
-	var slotLogger log.Logger
 
+	// in the future if we need to, we can create a map of key/values for which
+	// URI to create the slot under, but for now there is only one place, so
+	// make things as simple as possible.
 	awesome_mapper2.AddFunction("addslot", func(args ...interface{}) (interface{}, error) {
 		FQDD, ok := args[0].(string)
 		if !ok {
 			return nil, errors.New("Need a string fqdd for addslot(), but didnt get one")
 		}
-		slotLogger.Warn("called addslot from mapper", "FQDD", FQDD)
+		slotLogger.Debug("called addslot from mapper", "FQDD", FQDD)
 
 		s := strings.Split(FQDD, ".")
 		group, index := s[0], s[1]
@@ -55,13 +54,13 @@ func CreateSlotCollection(ctx context.Context, baseView viewer, cfgMgr *viper.Vi
 		if ok {
 			slotLogger.Warn("slot already created, skip", "baseSlotURI", myModParams()["collection_uri"], "SlotEntry.Id", FQDD)
 			slotsMu.Unlock()
-			return nil, nil
+			return false, nil
 		}
 		// track that this slot is instantiated
 		slots[FQDD] = struct{}{}
 		slotsMu.Unlock()
 
-		slotLogger.Warn("About to instantiate", "FQDD", FQDD)
+		slotLogger.Info("About to instantiate", "FQDD", FQDD)
 		// have to do this in a goroutine because awesome mapper is locked while it processes events
 		go instantiateSvc.InstantiateFromCfg(ctx, cfgMgr, "slot",
 			myModParams(map[string]interface{}{
@@ -70,11 +69,10 @@ func CreateSlotCollection(ctx context.Context, baseView viewer, cfgMgr *viper.Vi
 				"Index": index, // for ar mapper
 			}),
 		)
-		slotLogger.Warn("Created Slot", "SlotEntry.Id", FQDD)
 
 		return true, nil
 	})
 
 	slotLogger, _, _ = instantiateSvc.InstantiateFromCfg(ctx, cfgMgr, "slotcollection", myModParams())
-	slotLogger.Warn("Created slot collection", "uri", baseView.GetURI()+"/Slots")
+	slotLogger.Info("Created slot collection", "uri", baseView.GetURI()+"/Slots")
 }
