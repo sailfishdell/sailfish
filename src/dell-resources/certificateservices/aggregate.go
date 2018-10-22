d77608fcb1af24287cc80c58f27cc0f85fdd1dd9
Michael Brown <mebrown@michaels-house.net>
2018-10-21 21:31:45 -0500

    Remove places where we explicitly initialize collection to false

--- src/dell-resources/certificateservices/aggregate.go
+++ src/dell-resources/certificateservices/aggregate.go
@@ -12,20 +12,20 @@ import (
 func AddAggregate(ctx context.Context, v *view.View, baseUri string, ch eh.CommandHandler) (ret eh.UUID) {
 	ret = eh.NewUUID()
 
-  properties := map[string]interface{}{
-    "Id":                             "CertificateService",
-    "Name":                           "Certificate Service",
-    "Description":                    "Represents the properties of Certificate Service",
-    "CertificateSigningRequest@meta": v.Meta(view.PropGET("certificate_signing_request")),
-    "Actions": map[string]interface{}{
-      "#DellCertificateService.GenerateCSR": map[string]interface{}{
-        "target": v.GetActionURI("certificates.generatecsr"),
-      },
-    },
-    "CertificateInventory": map[string]interface{}{
-      "@odata.id": v.GetURI() + "/CertificateService/CertificateInventory",
-    },
-  }
+	properties := map[string]interface{}{
+		"Id":                             "CertificateService",
+		"Name":                           "Certificate Service",
+		"Description":                    "Represents the properties of Certificate Service",
+		"CertificateSigningRequest@meta": v.Meta(view.PropGET("certificate_signing_request")),
+		"Actions": map[string]interface{}{
+			"#DellCertificateService.GenerateCSR": map[string]interface{}{
+				"target": v.GetActionURI("certificates.generatecsr"),
+			},
+		},
+		"CertificateInventory": map[string]interface{}{
+			"@odata.id": v.GetURI() + "/CertificateService/CertificateInventory",
+		},
+	}
 
 	ch.HandleCommand(
 		ctx,
@@ -48,7 +48,6 @@ func AddAggregate(ctx context.Context, v *view.View, baseUri string, ch eh.Comma
 		ctx,
 		&domain.CreateRedfishResource{
 			ID:          eh.NewUUID(),
-			Collection:  false,
 			ResourceURI: baseUri + "/CertificateService/CertificateInventory",
 			Type:        "#DellCertificateInventoryCollection.DellCertificateInventoryCollection",
 			Context:     "/redfish/v1/$metadata#DellCertificateInventoryCollection.DellCertificateInventoryCollection",
@@ -60,36 +59,34 @@ func AddAggregate(ctx context.Context, v *view.View, baseUri string, ch eh.Comma
 				"DELETE": []string{},
 			},
 			Properties: map[string]interface{}{
-				"Name":        "Certificate Inventory Collection",
-				"Description": "Collection of Certificate Inventory",
-        "Members@meta":             v.Meta(view.GETProperty("certificate_uris"), view.GETFormatter("formatOdataList"), view.GETModel("default")),
-        "Members@odata.count@meta": v.Meta(view.GETProperty("certificate_uris"), view.GETFormatter("count"), view.GETModel("default")),
-
+				"Name":                     "Certificate Inventory Collection",
+				"Description":              "Collection of Certificate Inventory",
+				"Members@meta":             v.Meta(view.GETProperty("certificate_uris"), view.GETFormatter("formatOdataList"), view.GETModel("default")),
+				"Members@odata.count@meta": v.Meta(view.GETProperty("certificate_uris"), view.GETFormatter("count"), view.GETModel("default")),
 			}})
 
-   ch.HandleCommand(
-    ctx,
-    &domain.CreateRedfishResource{
-      ID:          eh.NewUUID(),
-      Collection:  false,
-      ResourceURI: baseUri + "/CertificateService/CertificateInventory/FactoryIdentity.1",
-      Type:        "#DellCertificateInventory.v1_0_0.DellCertificateInventory",
-      Context:     "/redfish/v1/$metadata#DellCertificateInventory.DellCertificateInventory",
-      Privileges:  map[string]interface{}{
-        "GET":    []string{"ConfigureManager"},
-        "POST":   []string{},
-        "PUT":    []string{},
-        "PATCH":  []string{},
-        "DELETE": []string{},
-      },
-      Properties: map[string]interface{}{
-        "Certificate@meta":   v.Meta(view.PropGET("certificate")),
-        "Description":        "Certificate Inventory Instance",
-        "DownloadFileFormat": "PEM",
-        "Id":                 "FactoryIdentity.1",
-        "Name":               "Factory Identity Certificate",
-        "Type":               "FactoryIdentity",
-      }})
+	ch.HandleCommand(
+		ctx,
+		&domain.CreateRedfishResource{
+			ID:          eh.NewUUID(),
+			ResourceURI: baseUri + "/CertificateService/CertificateInventory/FactoryIdentity.1",
+			Type:        "#DellCertificateInventory.v1_0_0.DellCertificateInventory",
+			Context:     "/redfish/v1/$metadata#DellCertificateInventory.DellCertificateInventory",
+			Privileges: map[string]interface{}{
+				"GET":    []string{"ConfigureManager"},
+				"POST":   []string{},
+				"PUT":    []string{},
+				"PATCH":  []string{},
+				"DELETE": []string{},
+			},
+			Properties: map[string]interface{}{
+				"Certificate@meta":   v.Meta(view.PropGET("certificate")),
+				"Description":        "Certificate Inventory Instance",
+				"DownloadFileFormat": "PEM",
+				"Id":                 "FactoryIdentity.1",
+				"Name":               "Factory Identity Certificate",
+				"Type":               "FactoryIdentity",
+			}})
 
 	return
 }
--- src/dell-resources/chassis/iom.slot/iomconfig/aggregate.go
+++ src/dell-resources/chassis/iom.slot/iomconfig/aggregate.go
@@ -15,7 +15,6 @@ func AddAggregate(ctx context.Context, logger log.Logger, v *view.View, ch eh.Co
 		ctx,
 		&domain.CreateRedfishResource{
 			ID:          v.GetUUID(),
-			Collection:  false,
 			ResourceURI: v.GetURI(),
 			Type:        "#DellIomConfiguration.v1_0_0.DellIomConfiguration",
 			Context:     "/redfish/v1/$metadata#ChassisCollection.ChassisCollection/Members/$entity",
@@ -27,10 +26,10 @@ func AddAggregate(ctx context.Context, logger log.Logger, v *view.View, ch eh.Co
 				"DELETE": []string{}, // can't be deleted
 			},
 			Properties: map[string]interface{}{
-				"Id@meta":                          v.Meta(view.PropGET("unique_name")),
-				"internal_mgmt_supported@meta":     v.Meta(view.PropGET("managed")),
-                "IOMConfig_objects@meta":           v.Meta(view.PropGET("config")),
-                "Capabilities@meta":                v.Meta(view.PropGET("capabilities")),
+				"Id@meta":                      v.Meta(view.PropGET("unique_name")),
+				"internal_mgmt_supported@meta": v.Meta(view.PropGET("managed")),
+				"IOMConfig_objects@meta":       v.Meta(view.PropGET("config")),
+				"Capabilities@meta":            v.Meta(view.PropGET("capabilities")),
 			}})
 
 }
