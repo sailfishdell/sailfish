e2e1c8be20895f2a9076c3d2dc6472fb56c61456
michael_e_brown <michael_e_brown@dell.com>
2018-10-30 08:07:47 -0500

    Share the awesome mapper govaluate function list with the instantiate instances

--- src/ocp/testaggregate/functions.go
+++ src/ocp/testaggregate/functions.go
@@ -2,21 +2,26 @@ package testaggregate
 
 import (
 	"github.com/Knetic/govaluate"
+	"github.com/superchalupa/sailfish/src/ocp/awesome_mapper2"
 )
 
 var functions map[string]govaluate.ExpressionFunction
 
 func init() {
-	functions = map[string]govaluate.ExpressionFunction{
-		"array": func(args ...interface{}) (interface{}, error) {
+	// share our functions with awesome mapper
+	functions = awesome_mapper2.InitFunctions()
+
+	awesome_mapper2.AddFunction("array",
+		func(args ...interface{}) (interface{}, error) {
 			a := []interface{}{}
 			for _, i := range args {
 				a = append(a, i)
 			}
 			return a, nil
-		},
-		"add_attribute_property": func(args ...interface{}) (interface{}, error) {
+		})
+
+	awesome_mapper2.AddFunction("add_attribute_property",
+		func(args ...interface{}) (interface{}, error) {
 			return map[string]map[string]map[string]interface{}{}, nil
-		},
-	}
+		})
 }
