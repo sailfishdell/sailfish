a11f89a09e1c875dcd55a296ed31f9ef0930cc76
michael_e_brown <michael_e_brown@dell.com>
2018-11-01 06:16:04 -0500

    Most of our crit logs are actually errors, fix. Add abiliy to pass through parameters to underlying mapper from the config.

--- src/ocp/testaggregate/registry.go
+++ src/ocp/testaggregate/registry.go
@@ -31,40 +31,40 @@ func RegisterPumpAction(s *Service, actionSvc actionService, pumpSvc pumpService
 	s.RegisterViewFunction("with_PumpHandledAction", func(ctx context.Context, logger log.Logger, cfgMgr *viper.Viper, cfgMgrMu *sync.RWMutex, vw *view.View, cfg interface{}, parameters map[string]interface{}) error {
 		cfgParams, ok := cfg.(map[interface{}]interface{})
 		if !ok {
-			logger.Crit("Failed to type assert cfg to string", "cfg", cfg)
+			logger.Error("Failed to type assert cfg to string", "cfg", cfg)
 			return errors.New("Failed to type assert expression to string")
 		}
 
 		actionName, ok := cfgParams["name"]
 		if !ok {
-			logger.Crit("Config file missing action name for action", "cfg", cfg)
+			logger.Error("Config file missing action name for action", "cfg", cfg)
 			return nil
 		}
 		actionNameStr, ok := actionName.(string)
 		if !ok {
-			logger.Crit("Action name isnt a string", "cfg", cfg)
+			logger.Error("Action name isnt a string", "cfg", cfg)
 			return nil
 		}
 
 		actionURIFrag, ok := cfgParams["uri"]
 		if !ok {
-			logger.Crit("Config file missing action URI for action", "cfg", cfg)
+			logger.Error("Config file missing action URI for action", "cfg", cfg)
 			return nil
 		}
 		actionURIFragStr, ok := actionURIFrag.(string)
 		if !ok {
-			logger.Crit("Action URI isnt a string", "cfg", cfg)
+			logger.Error("Action URI isnt a string", "cfg", cfg)
 			return nil
 		}
 
 		actionTimeout, ok := cfgParams["timeout"]
 		if !ok {
-			logger.Crit("Config file missing action URI for action", "cfg", cfg)
+			logger.Error("Config file missing action URI for action", "cfg", cfg)
 			return nil
 		}
 		actionTimeoutInt, ok := actionTimeout.(int)
 		if !ok {
-			logger.Crit("Action timeout isn't a number", "cfg", cfg)
+			logger.Error("Action timeout isn't a number", "cfg", cfg)
 			return nil
 		}
 
@@ -77,55 +77,55 @@ func RegisterPumpAction(s *Service, actionSvc actionService, pumpSvc pumpService
 	s.RegisterViewFunction("WithAction", func(ctx context.Context, logger log.Logger, cfgMgr *viper.Viper, cfgMgrMu *sync.RWMutex, vw *view.View, cfg interface{}, parameters map[string]interface{}) error {
 		cfgParams, ok := cfg.(map[interface{}]interface{})
 		if !ok {
-			logger.Crit("Failed to type assert cfg to string", "cfg", cfg)
+			logger.Error("Failed to type assert cfg to string", "cfg", cfg)
 			return errors.New("Failed to type assert expression to string")
 		}
 
 		actionName, ok := cfgParams["name"]
 		if !ok {
-			logger.Crit("Config file missing action name for action", "cfg", cfg)
+			logger.Error("Config file missing action name for action", "cfg", cfg)
 			return fmt.Errorf("Config file missing action name for action: %s", cfgParams)
 		}
 		actionNameStr, ok := actionName.(string)
 		if !ok {
-			logger.Crit("Action name isnt a string", "cfg", cfg)
+			logger.Error("Action name isnt a string", "cfg", cfg)
 			return nil
 		}
 
 		actionURIFrag, ok := cfgParams["uri"]
 		if !ok {
-			logger.Crit("Config file missing action URI for action", "cfg", cfg)
+			logger.Error("Config file missing action URI for action", "cfg", cfg)
 			return nil
 		}
 		actionURIFragStr, ok := actionURIFrag.(string)
 		if !ok {
-			logger.Crit("Action URI isnt a string", "cfg", cfg)
+			logger.Error("Action URI isnt a string", "cfg", cfg)
 			return nil
 		}
 
 		modelAction, ok := cfgParams["actionFunction"]
 		if !ok {
-			logger.Crit("Config file missing model name for action", "cfg", cfg)
+			logger.Error("Config file missing model name for action", "cfg", cfg)
 			return errors.New("Config file missing model name for action")
 		}
 		modelActionStr, ok := modelAction.(string)
 		if !ok {
-			logger.Crit("model name isnt a string", "cfg", cfg)
+			logger.Error("model name isnt a string", "cfg", cfg)
 			return errors.New("model name isnt a string")
 		}
 		expr, err := govaluate.NewEvaluableExpressionWithFunctions(modelActionStr, functions)
 		if err != nil {
-			logger.Crit("Failed to create evaluable expression", "expr", expr, "err", err)
+			logger.Error("Failed to create evaluable expression", "expr", expr, "err", err)
 			return errors.New("Failed to create evaluable expression")
 		}
 		action, err := expr.Evaluate(parameters)
 		if err != nil {
-			logger.Crit("expression evaluation failed", "expr", expr, "err", err)
+			logger.Error("expression evaluation failed", "expr", expr, "err", err)
 			return errors.New("expression evaluation failed")
 		}
 		actionFn, ok := action.(view.Action)
 		if !ok {
-			logger.Crit("Could not type assert to action function", "expr", expr)
+			logger.Error("Could not type assert to action function", "expr", expr)
 			return errors.New("Could not type assert to action function")
 		}
 
@@ -138,39 +138,39 @@ func RegisterPumpAction(s *Service, actionSvc actionService, pumpSvc pumpService
 	s.RegisterViewFunction("withModel", func(ctx context.Context, logger log.Logger, cfgMgr *viper.Viper, cfgMgrMu *sync.RWMutex, vw *view.View, cfg interface{}, parameters map[string]interface{}) error {
 		cfgParams, ok := cfg.(map[interface{}]interface{})
 		if !ok {
-			logger.Crit("Failed to type assert cfg to string", "cfg", cfg)
+			logger.Error("Failed to type assert cfg to string", "cfg", cfg)
 			return errors.New("Failed to type assert expression to string")
 		}
 
 		modelName, ok := cfgParams["name"]
 		if !ok {
-			logger.Crit("Config file missing model name for action", "cfg", cfg)
+			logger.Error("Config file missing model name for action", "cfg", cfg)
 			return nil
 		}
 		modelNameStr, ok := modelName.(string)
 		if !ok {
-			logger.Crit("model name isnt a string", "cfg", cfg)
+			logger.Error("model name isnt a string", "cfg", cfg)
 			return nil
 		}
 
 		modelExpr, ok := cfgParams["expr"]
 		if !ok {
-			logger.Crit("Config file missing model name for action", "cfg", cfg)
+			logger.Error("Config file missing model name for action", "cfg", cfg)
 			return nil
 		}
 		modelExprStr, ok := modelExpr.(string)
 		if !ok {
-			logger.Crit("model name isnt a string", "cfg", cfg)
+			logger.Error("model name isnt a string", "cfg", cfg)
 			return nil
 		}
 		expr, err := govaluate.NewEvaluableExpressionWithFunctions(modelExprStr, functions)
 		if err != nil {
-			logger.Crit("Failed to create evaluable expression", "expr", expr, "err", err)
+			logger.Error("Failed to create evaluable expression", "expr", expr, "err", err)
 			return errors.New("Failed to create evaluable expression")
 		}
 		modelVar, err := expr.Evaluate(parameters)
 		if err != nil {
-			logger.Crit("expression evaluation failed", "expr", expr, "err", err)
+			logger.Error("expression evaluation failed", "expr", expr, "err", err)
 			return errors.New("expression evaluation failed")
 		}
 		logger.Info("WithModel", "name", modelName, "exprStr", modelExprStr)
@@ -182,29 +182,29 @@ func RegisterPumpAction(s *Service, actionSvc actionService, pumpSvc pumpService
 	s.RegisterViewFunction("linkModel", func(ctx context.Context, logger log.Logger, cfgMgr *viper.Viper, cfgMgrMu *sync.RWMutex, vw *view.View, cfg interface{}, parameters map[string]interface{}) error {
 		cfgParams, ok := cfg.(map[interface{}]interface{})
 		if !ok {
-			logger.Crit("Failed to type assert cfg to string", "cfg", cfg)
+			logger.Error("Failed to type assert cfg to string", "cfg", cfg)
 			return errors.New("Failed to type assert expression to string")
 		}
 
 		existing, ok := cfgParams["existing"]
 		if !ok {
-			logger.Crit("Config file missing model name for action", "cfg", cfg)
+			logger.Error("Config file missing model name for action", "cfg", cfg)
 			return nil
 		}
 		existingStr, ok := existing.(string)
 		if !ok {
-			logger.Crit("model name isnt a string", "cfg", cfg)
+			logger.Error("model name isnt a string", "cfg", cfg)
 			return nil
 		}
 
 		linkname, ok := cfgParams["linkname"]
 		if !ok {
-			logger.Crit("Config file missing model name for action", "cfg", cfg)
+			logger.Error("Config file missing model name for action", "cfg", cfg)
 			return nil
 		}
 		linknameStr, ok := linkname.(string)
 		if !ok {
-			logger.Crit("model name isnt a string", "cfg", cfg)
+			logger.Error("model name isnt a string", "cfg", cfg)
 			return nil
 		}
 
@@ -217,7 +217,7 @@ func RegisterPumpAction(s *Service, actionSvc actionService, pumpSvc pumpService
 	s.RegisterViewFunction("etag", func(ctx context.Context, logger log.Logger, cfgMgr *viper.Viper, cfgMgrMu *sync.RWMutex, vw *view.View, cfg interface{}, parameters map[string]interface{}) error {
 		cfgParams, ok := cfg.([]interface{})
 		if !ok {
-			logger.Crit("Failed to type assert cfg to string", "cfg", cfg)
+			logger.Error("Failed to type assert cfg to string", "cfg", cfg)
 			return errors.New("Failed to type assert expression to string")
 		}
 
@@ -240,22 +240,22 @@ func RegisterWithURI(s *Service) {
 	s.RegisterViewFunction("with_URI", func(ctx context.Context, logger log.Logger, cfgMgr *viper.Viper, cfgMgrMu *sync.RWMutex, vw *view.View, cfg interface{}, parameters map[string]interface{}) error {
 		exprStr, ok := cfg.(string)
 		if !ok {
-			logger.Crit("Failed to type assert cfg to string", "cfg", cfg)
+			logger.Error("Failed to type assert cfg to string", "cfg", cfg)
 			return errors.New("Failed to type assert expression to string")
 		}
 		expr, err := govaluate.NewEvaluableExpressionWithFunctions(exprStr, functions)
 		if err != nil {
-			logger.Crit("Failed to create evaluable expression", "expr", expr, "err", err)
+			logger.Error("Failed to create evaluable expression", "expr", expr, "err", err)
 			return errors.New("Failed to create evaluable expression")
 		}
 		uri, err := expr.Evaluate(parameters)
 		if err != nil {
-			logger.Crit("expression evaluation failed", "expr", expr, "err", err)
+			logger.Error("expression evaluation failed", "expr", expr, "err", err)
 			return errors.New("expression evaluation failed")
 		}
 		uriStr, ok := uri.(string)
 		if !ok {
-			logger.Crit("expression returned non-string", "exprStr", exprStr)
+			logger.Error("expression returned non-string", "exprStr", exprStr)
 			return errors.New("expression returned non-string")
 		}
 
@@ -270,7 +270,7 @@ func RegisterPublishEvents(s *Service, evtSvc EventService) {
 	s.RegisterViewFunction("PublishResourceUpdatedEventsForModel", func(ctx context.Context, logger log.Logger, cfgMgr *viper.Viper, cfgMgrMu *sync.RWMutex, vw *view.View, cfg interface{}, parameters map[string]interface{}) error {
 		modelName, ok := cfg.(string)
 		if !ok {
-			logger.Crit("Failed to type assert cfg to string", "cfg", cfg)
+			logger.Error("Failed to type assert cfg to string", "cfg", cfg)
 			return errors.New("Failed to type assert expression to string")
 		}
 
@@ -285,7 +285,7 @@ func RegisterAM2(s *Service, am2Svc *am2.Service) {
 	s.RegisterControllerFunction("AM2", func(ctx context.Context, logger log.Logger, cfgMgr *viper.Viper, cfgMgrMu *sync.RWMutex, vw *view.View, cfg interface{}, parameters map[string]interface{}) error {
 		cfgParams, ok := cfg.(map[interface{}]interface{})
 		if !ok {
-			logger.Crit("Failed to type assert cfg to string", "cfg", cfg)
+			logger.Error("Failed to type assert cfg to string", "cfg", cfg)
 			return errors.New("Failed to type assert expression to string")
 		}
 
@@ -301,42 +301,80 @@ func RegisterAM2(s *Service, am2Svc *am2.Service) {
 
 		cfgSection, ok := cfgParams["cfgsection"]
 		if !ok {
-			logger.Crit("Required parameter 'cfgsection' missing, cannot continue")
+			logger.Error("Required parameter 'cfgsection' missing, cannot continue")
 			return errors.New("Required parameter 'cfgsection' missing, cannot continue")
 		}
 		cfgSectionStr, ok := cfgSection.(string)
 		if !ok {
-			logger.Crit("Required parameter 'cfgsection' could not be cast to string")
+			logger.Error("Required parameter 'cfgsection' could not be cast to string")
 			return errors.New("Required parameter 'cfgsection' could not be cast to string")
 		}
 
 		uniqueName, ok := cfgParams["uniquename"]
 		if !ok {
-			logger.Crit("Required parameter 'uniquename' missing, cannot continue")
+			logger.Error("Required parameter 'uniquename' missing, cannot continue")
 			return errors.New("Required parameter 'uniquename' missing, cannot continue")
 		}
 		uniqueNameStr, ok := uniqueName.(string)
 		if !ok {
-			logger.Crit("Required parameter 'uniquename' could not be cast to string")
+			logger.Error("Required parameter 'uniquename' could not be cast to string")
 			return errors.New("Required parameter 'uniquename' could not be cast to string")
 		}
 
 		expr, err := govaluate.NewEvaluableExpressionWithFunctions(uniqueNameStr, functions)
 		if err != nil {
-			logger.Crit("Failed to create evaluable expression", "uniqueNameStr", uniqueNameStr, "err", err)
+			logger.Error("Failed to create evaluable expression", "uniqueNameStr", uniqueNameStr, "err", err)
 			return err
 		}
 		uniqueName, err = expr.Evaluate(parameters)
 		if err != nil {
-			logger.Crit("expression evaluation failed", "expr", expr, "err", err, "cfgSection", cfgSectionStr, "uniqueName", uniqueNameStr)
+			logger.Error("expression evaluation failed", "expr", expr, "err", err, "cfgSection", cfgSectionStr, "uniqueName", uniqueNameStr)
 			return err
 		}
 
 		uniqueNameStr, ok = uniqueName.(string)
 		if err != nil {
-			logger.Crit("could not cast result to string", "uniqueName", uniqueName)
+			logger.Error("could not cast result to string", "uniqueName", uniqueName)
 			return errors.New("could not cast result to string")
 		}
+
+		// if this stuff not present, no big deal
+		func() {
+			passthruParams, ok := cfgParams["passthru"]
+			if !ok {
+				return
+			}
+			passthruParamsMap, ok := passthruParams.(map[interface{}]interface{})
+			if !ok {
+				logger.Error("Optional parameter 'passthru' could not be cast to string", "type", fmt.Sprintf("%T", passthruParams))
+				return
+			}
+
+			for k, v := range passthruParamsMap {
+				keyStr, ok := k.(string)
+				if !ok {
+					logger.Error("expression could not be cast to string", "k", k, "cfgSection", cfgSectionStr, "passthruParams", uniqueNameStr)
+				}
+
+				exprStr, ok := v.(string)
+				if !ok {
+					logger.Error("expression could not be cast to string", "v", v, "cfgSection", cfgSectionStr, "passthruParams", uniqueNameStr)
+				}
+
+				expr, err := govaluate.NewEvaluableExpressionWithFunctions(exprStr, functions)
+				if err != nil {
+					logger.Error("Failed to create evaluable expression", "passthruParamsMap", passthruParamsMap, "err", err)
+					continue
+				}
+				val, err := expr.Evaluate(parameters)
+				if err != nil {
+					logger.Error("expression evaluation failed", "expr", expr, "err", err, "cfgSection", cfgSectionStr, "passthruParams", uniqueNameStr)
+					continue
+				}
+
+				parameters[keyStr] = val
+			}
+		}()
 
 		logger.Debug("Creating awesome_mapper2 controller", "modelName", modelNameStr, "cfgSection", cfgSectionStr, "uniqueName", uniqueNameStr)
 		am2Svc.NewMapping(ctx, logger, cfgMgr, cfgMgrMu, vw.GetModel(modelNameStr), cfgSectionStr, uniqueNameStr, parameters)
