202692b56f49732dd832ea046c65f23c5fd51ce8
mahesh_babu_ramaiah <mahesh_babu_ramaiah@dell.com>
2018-11-09 08:33:43 -0600

    BITS0000: Add handling to add storage enclosure dynamically
    Change-Id: Idd1ec1892deb1cc1f349645ec66527db5dfb9160

--- src/dell-idrac/chassis.go
+++ src/dell-idrac/chassis.go
@@ -35,8 +35,41 @@ func AddChassisInstantiate(l log.Logger, instantiateSvc *testaggregate.Service) 
 		l.Info("About to instantiate", "FQDD", FQDD)
 
 		// have to do this in a goroutine because awesome mapper is locked while it processes events
-		go instantiateSvc.Instantiate("system_chassis", map[string]interface{}{"FQDD": "System.Chassis.1"})
+		go instantiateSvc.Instantiate("system_chassis", map[string]interface{}{"FQDD": FQDD})
 
 		return true, nil
 	})
+
+
+	// create hash and lock to keep track of the slots we instantiate
+	storageEnclosureListMu := sync.Mutex{}
+	storageEnclosureList := map[string]struct{}{}
+
+	awesome_mapper2.AddFunction("addstorageenclosure", func(args ...interface{}) (interface{}, error) {
+		FQDD, ok := args[0].(string)
+		if !ok {
+			return nil, errors.New("Need a string fqdd for addslot(), but didnt get one")
+		}
+		l.Debug("called addslot from mapper", "FQDD", FQDD)
+
+		storageEnclosureListMu.Lock()
+		_, ok = storageEnclosureList[FQDD]
+		if ok {
+			l.Warn("Storage Enclosure already created, skip", "CHASSIS FQDD", FQDD)
+			storageEnclosureListMu.Unlock()
+			return false, nil
+		}
+		// track that this slot is instantiated
+		storageEnclosureList[FQDD] = struct{}{}
+		storageEnclosureListMu.Unlock()
+
+		l.Info("About to instantiate", "FQDD", FQDD)
+
+		// have to do this in a goroutine because awesome mapper is locked while it processes events
+		go instantiateSvc.Instantiate("storage_enclosure", map[string]interface{}{"URI_FQDD": FQDD, "EVENT_FQDD": "308|C|" + FQDD})
+
+		return true, nil
+	})
+
+
 }
