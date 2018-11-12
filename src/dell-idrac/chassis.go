123fdc44a5ced667e6ba0fee8d68706bd84446bc
Michael Brown <mebrown@michaels-house.net>
2018-11-11 22:58:00 -0600

    Make a generic function to create awesome mapper functions to instantiate various things
    Change-Id: Ibffe25fcbcbc96803f28a02bb1c68446eeeed27f

--- src/dell-idrac/chassis.go
+++ src/dell-idrac/chassis.go
@@ -9,67 +9,54 @@ import (
 	"github.com/superchalupa/sailfish/src/ocp/testaggregate"
 )
 
-func AddChassisInstantiate(l log.Logger, instantiateSvc *testaggregate.Service) {
-	// create hash and lock to keep track of the slots we instantiate
-	chassisListMu := sync.Mutex{}
-	chassisList := map[string]struct{}{}
+func MakeMaker(l log.Logger, name string, fn func(args ...interface{}) (interface{}, error)) {
+	// create hash and lock to keep track of the things we instantiate
+	setMu := sync.Mutex{}
+	set := map[string]struct{}{}
 
-	awesome_mapper2.AddFunction("addsystemchassis", func(args ...interface{}) (interface{}, error) {
+	awesome_mapper2.AddFunction("add"+name, func(args ...interface{}) (interface{}, error) {
+		uniqueName, ok := args[0].(string)
+		if !ok {
+			return nil, errors.New("Need a string fqdd for addslot(), but didnt get one")
+		}
+		l.Debug("called add"+name+" from mapper", "uniqueName", uniqueName)
+
+		setMu.Lock()
+		_, ok = set[uniqueName]
+		if ok {
+			l.Warn("Already created unique name in this set", "name", name, "uniqueName", uniqueName)
+			setMu.Unlock()
+			return false, nil
+		}
+		// track that this slot is instantiated
+		set[uniqueName] = struct{}{}
+		setMu.Unlock()
+
+		l.Info("About to instantiate", "uniqueName", uniqueName)
+		return fn(args...)
+	})
+}
+
+func AddChassisInstantiate(l log.Logger, instantiateSvc *testaggregate.Service) {
+	MakeMaker(l, "system_chassis", func(args ...interface{}) (interface{}, error) {
 		FQDD, ok := args[0].(string)
 		if !ok {
 			return nil, errors.New("Need a string fqdd for addslot(), but didnt get one")
 		}
-		l.Debug("called addslot from mapper", "FQDD", FQDD)
-
-		chassisListMu.Lock()
-		_, ok = chassisList[FQDD]
-		if ok {
-			l.Warn("Chassis already created, skip", "CHASSIS FQDD", FQDD)
-			chassisListMu.Unlock()
-			return false, nil
-		}
-		// track that this slot is instantiated
-		chassisList[FQDD] = struct{}{}
-		chassisListMu.Unlock()
-
-		l.Info("About to instantiate", "FQDD", FQDD)
-
 		// have to do this in a goroutine because awesome mapper is locked while it processes events
 		go instantiateSvc.Instantiate("system_chassis", map[string]interface{}{"FQDD": FQDD})
 
 		return true, nil
 	})
 
-
-	// create hash and lock to keep track of the slots we instantiate
-	storageEnclosureListMu := sync.Mutex{}
-	storageEnclosureList := map[string]struct{}{}
-
-	awesome_mapper2.AddFunction("addstorageenclosure", func(args ...interface{}) (interface{}, error) {
+	MakeMaker(l, "storage_enclosure", func(args ...interface{}) (interface{}, error) {
 		FQDD, ok := args[0].(string)
 		if !ok {
 			return nil, errors.New("Need a string fqdd for addslot(), but didnt get one")
 		}
-		l.Debug("called addslot from mapper", "FQDD", FQDD)
-
-		storageEnclosureListMu.Lock()
-		_, ok = storageEnclosureList[FQDD]
-		if ok {
-			l.Warn("Storage Enclosure already created, skip", "CHASSIS FQDD", FQDD)
-			storageEnclosureListMu.Unlock()
-			return false, nil
-		}
-		// track that this slot is instantiated
-		storageEnclosureList[FQDD] = struct{}{}
-		storageEnclosureListMu.Unlock()
-
-		l.Info("About to instantiate", "FQDD", FQDD)
-
 		// have to do this in a goroutine because awesome mapper is locked while it processes events
 		go instantiateSvc.Instantiate("storage_enclosure", map[string]interface{}{"URI_FQDD": FQDD, "EVENT_FQDD": "308|C|" + FQDD})
 
 		return true, nil
 	})
-
-
 }
