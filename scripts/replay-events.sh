25f43946eacf6dd50635812b65a5fd050c28d0c2
michael_e_brown <michael_e_brown@dell.com>
2018-11-19 20:21:57 -0600

    BITS0000: update replay script to accept multiple filenames on the cli.
    Change-Id: I9e7080613a479dde3472d5fff6d80a844d990bec

--- scripts/replay-events.sh
+++ scripts/replay-events.sh
@@ -17,16 +17,19 @@ fi
 tmpfile=$(mktemp ./TEMP-XXXXXX)
 trap "rm -f $tmpfile" EXIT
 
-while read -u 5 line ; do
-   echo "$line" > $tmpfile
-    $CURLCMD --fail -f $BASE/api/Event%3AInject -d  @$tmpfile
+for file in "$@"
+do
+  while read -u 5 line ; do
+     echo "$line" > $tmpfile
+      $CURLCMD --fail -f $BASE/api/Event%3AInject -d  @$tmpfile
 
-    if [ -n "$singlestep" ]; then  read -p "Paused" pause; fi
+      if [ -n "$singlestep" ]; then  read -p "Paused" pause; fi
 
-# rate limit requests, if needed:
-#    i=$((i+1))
-#    if [ $((i%20)) -eq 0 ] ; then
-#        sleep 1
-#    fi
+  # rate limit requests, if needed:
+  #    i=$((i+1))
+  #    if [ $((i%20)) -eq 0 ] ; then
+  #        sleep 1
+  #    fi
 
-done 5<$1
+  done 5<$file
+done
