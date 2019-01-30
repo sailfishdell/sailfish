ec18a58027c9d52b56042ce55219dca1c732f700
michael_e_brown <michael_e_brown@dell.com>
2019-01-29 19:59:02 -0600

    BITS0000: more memory usage reduction
    Change-Id: I4d6c1c666703c4a73c587ce74c8578c131526db9

--- src/redfishresource/http_get.go
+++ src/redfishresource/http_get.go
@@ -2,13 +2,15 @@ package domain
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	eh "github.com/looplab/eventhorizon"
 )
 
 const (
-	GETCommand = eh.CommandType("http:RedfishResource:GET")
+	GETCommand       = eh.CommandType("http:RedfishResource:GET")
+	DefaultCacheTime = 20
 )
 
 // Static type checking for commands to prevent runtime errors due to typos
@@ -19,6 +21,7 @@ type GET struct {
 	ID           eh.UUID `json:"id"`
 	CmdID        eh.UUID `json:"cmdid"`
 	HTTPEventBus eh.EventBus
+	outChan      chan<- eh.Event
 }
 
 func (c *GET) AggregateType() eh.AggregateType { return AggregateType }
@@ -26,6 +29,11 @@ func (c *GET) AggregateID() eh.UUID            { return c.ID }
 func (c *GET) CommandType() eh.CommandType     { return GETCommand }
 func (c *GET) SetAggID(id eh.UUID)             { c.ID = id }
 func (c *GET) SetCmdID(id eh.UUID)             { c.CmdID = id }
+
+func (c *GET) UseEventChan(out chan<- eh.Event) {
+	c.outChan = out
+}
+
 func (c *GET) SetUserDetails(u string, p []string) string {
 	return "checkMaster"
 }
@@ -36,12 +44,56 @@ func (c *GET) Handle(ctx context.Context, a *RedfishResourceAggregate) error {
 		StatusCode: 200,
 	}
 	// TODO: Should be able to discern supported methods from the meta and return those
-
-	data.Results, _ = ProcessGET(ctx, &a.Properties, &a.Authorization)
-
 	// TODO: set error status code based on err from ProcessGET
 	data.Headers = a.Headers
 
-	c.HTTPEventBus.PublishEvent(ctx, eh.NewEvent(HTTPCmdProcessed, data, time.Now()))
+	a.ResultsCacheMu.RLock()
+	if a.ResultsCache != nil {
+		fmt.Printf(".")
+		data.Results = a.ResultsCache
+		a.ResultsCacheMu.RUnlock()
+		if c.outChan != nil {
+			c.outChan <- eh.NewEvent(HTTPCmdProcessed, data, time.Now())
+		} else {
+			c.HTTPEventBus.PublishEvent(ctx, eh.NewEvent(HTTPCmdProcessed, data, time.Now()))
+		}
+		return nil
+	}
+	a.ResultsCacheMu.RUnlock()
+
+	a.ResultsCacheMu.Lock()
+	if a.ResultsCache == nil {
+		fmt.Printf("X")
+		data.Results, _ = ProcessGET(ctx, &a.Properties, &a.Authorization)
+		a.ResultsCache = data.Results
+
+		// TODO: cache until an invalidation message comes in
+		// TODO: release cache if not used for a while
+		// TODO: heartbeat cache to indicate use
+
+		// simplest possible solution for now
+		go func(cacheTime int) {
+			if cacheTime == 0 {
+				cacheTime = DefaultCacheTime
+			}
+			select {
+			case <-time.After(time.Duration(cacheTime) * time.Second):
+				fmt.Printf("E")
+				a.ResultsCacheMu.Lock()
+				a.ResultsCache = nil
+				a.ResultsCacheMu.Unlock()
+			}
+		}(a.CacheTimeSec)
+	} else {
+		fmt.Printf("O")
+		data.Results = a.ResultsCache
+	}
+	a.ResultsCacheMu.Unlock()
+
+	if c.outChan != nil {
+		c.outChan <- eh.NewEvent(HTTPCmdProcessed, data, time.Now())
+	} else {
+		c.HTTPEventBus.PublishEvent(ctx, eh.NewEvent(HTTPCmdProcessed, data, time.Now()))
+	}
 	return nil
 }
