e45c41df8b624ad37e548e6988068f2e3b802adc
michael_e_brown <michael_e_brown@dell.com>
2019-02-07 12:59:59 -0600

    BITS0000: fixup filter for messageArgs
    Change-Id: Id91b7b8f59a550537ef9c652ecbe1b7123f74aa1

--- src/redfishresource/redfish_handler.go
+++ src/redfishresource/redfish_handler.go
@@ -524,11 +524,12 @@ func processFilterOneObject(memberInstance map[string]interface{}, filterArray [
 				}
 			}
 		//========= String array members =======
-		case []string:
+		case []interface{}:
 			//For this one, assume we're not going to find it until proven wrong
 			//If we drop out of the loop without finding it, the we don't keep it
+			localMemberStr := localMember.(string)
 			for b := 0; b < len(localMember) && !keepElement; b += 1 {
-				keepElement = strings.Contains(localMember[b], filterArray[j].SearchTerm)
+				keepElement = strings.Contains(localMemberStr[b], filterArray[j].SearchTerm)
 			}
 		//========= Time members =======
 		case time.Time:
