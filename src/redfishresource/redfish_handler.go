90d45ca0d887afa6dee3b8259f2a677ee9a6eeb7
henry_pang <henry_pang@dell.com>
2019-03-20 16:37:06 +0000

    JIT-0001 missing AttributeRegistry value in select
    Change-Id: I2c0b6f62e3b926f15d14a62704d4206e8b79c1f4

--- src/redfishresource/redfish_handler.go
+++ src/redfishresource/redfish_handler.go
@@ -812,6 +812,7 @@ func handleSelect(r *http.Request, d *HTTPCmdProcessedData) *HTTPCmdProcessedDat
 			makesel(&selectQuery, []string{"Name"})
 			makesel(&selectQuery, []string{"Description"})
 			makesel(&selectQuery, []string{"@*"})
+      makesel(&selectQuery, []string{"AttributeRegistry"})
 		}
 	}
 
