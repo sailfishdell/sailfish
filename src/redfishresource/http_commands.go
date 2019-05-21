885323d0a01fd7ff1d9a64c9b4c69c11a3b18836
michael_e_brown <michael_e_brown@dell.com>
2019-05-21 16:31:22 -0500

    BITS0000: memory usage reduction - reduce constant overhead in aggregate plugin name by shortening it, and not setting it when not needed.
    Change-Id: Ie906efed9b47c876ffb3db957c240f2519909eaf

--- src/redfishresource/http_commands.go
+++ src/redfishresource/http_commands.go
@@ -23,13 +23,14 @@ func init() {
 }
 
 const (
-	DELETECommand = eh.CommandType("http:RedfishResource:DELETE")
+	// All of these shortened from "http:RedfishResource:HTTP" to "R:HTTP" to save memory in the aggregate since this is the most common type
+	DELETECommand = eh.CommandType("R:DELETE")
 
-	PUTCommand     = eh.CommandType("http:RedfishResource:PUT")
-	PATCHCommand   = eh.CommandType("http:RedfishResource:PATCH")
-	POSTCommand    = eh.CommandType("http:RedfishResource:POST")
-	HEADCommand    = eh.CommandType("http:RedfishResource:HEAD")
-	OPTIONSCommand = eh.CommandType("http:RedfishResource:OPTIONS")
+	PUTCommand     = eh.CommandType("R:PUT")
+	PATCHCommand   = eh.CommandType("R:PATCH")
+	POSTCommand    = eh.CommandType("R:POST")
+	HEADCommand    = eh.CommandType("R:HEAD")
+	OPTIONSCommand = eh.CommandType("R:OPTIONS")
 )
 
 // Static type checking for commands to prevent runtime errors due to typos
