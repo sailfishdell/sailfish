e41cc39c214fdf5b16e05cac6303608cf4a878a5
heidi <heidi_maeder@dell.com>
2019-05-29 14:39:07 +0000

    Revert "JIT-133501:  if System.Chassis.1#FanSlot.X#Occupied is Unknown, Occupied is removed from /redfish/v1/Chassis/System.Chassis.1/Slots/FanSlot.X, if not Unknown, Occupied is added to the URL."
    This reverts commit cd22cf0697c653729baa3c526aacce0739b778ac.
    
    Change-Id: I716d6595bc94d1b22a317f2711d2940e74116c62

--- src/dell-ec/ec.go
+++ src/dell-ec/ec.go
@@ -98,7 +98,7 @@ func New(ctx context.Context, logger log.Logger, cfgMgr *viper.Viper, cfgMgrMu *
 	RegisterCertAggregate(instantiateSvc)
 	AddECInstantiate(logger, instantiateSvc)
 	initLCL(logger, instantiateSvc, ch, d)
-	initThermalSensor(ctx, logger, instantiateSvc, ch, d)
+	initThermalSensor(logger, instantiateSvc, ch, d)
 	inithealth(ctx, logger, ch, d)
 	initpowercontrol(logger)
 	stdmeta.InitializeSsoinfo(d)
