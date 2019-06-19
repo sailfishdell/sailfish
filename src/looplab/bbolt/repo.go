c06fed4e2a90bdaa9852dbe985ab68f0e16a5895
michael_e_brown <michael_e_brown@dell.com>
2019-06-19 11:40:18 -0500

    BITS0000: add bbolt database repo support
    Change-Id: Ib3e38cc19e98a997994281f3f73c7d939a591e7f

--- src/looplab/bbolt/repo.go
+++ src/looplab/bbolt/repo.go
@@ -1,50 +1,33 @@
-// Copyright (c) 2014 - The Event Horizon authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package memory
 
 import (
+	"bytes"
 	"context"
-	"sync"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"os"
 
 	eh "github.com/looplab/eventhorizon"
 
 	bbolt "github.com/etcd-io/bbolt"
 )
 
-var _ = bbolt.Open
-
 type namespace string
 
 // Repo implements an in memory repository of read models.
 type Repo struct {
-	// The outer map is with namespace as key, the inner with aggregate ID.
-	db   map[namespace]map[eh.UUID]eh.Entity
-	dbMu sync.RWMutex
-
-	// A list of all item ids, only the order is used.
-	// The outer map is for the namespace.
-	ids map[namespace][]eh.UUID
+	db *bbolt.DB
 }
 
 // NewRepo creates a new Repo.
 func NewRepo() *Repo {
-	r := &Repo{
-		ids: map[namespace][]eh.UUID{},
-		db:  map[namespace]map[eh.UUID]eh.Entity{},
+	os.Remove("./emmc_db.db")
+	t, err := bbolt.Open("emmc_db.db", 0600, nil)
+	if err != nil {
+		panic("Failed to start bbolt DB")
 	}
-	return r
+	return &Repo{db: t}
 }
 
 // Parent implements the Parent method of the eventhorizon.ReadRepo interface.
@@ -53,42 +36,64 @@ func (r *Repo) Parent() eh.ReadRepo {
 }
 
 // Find implements the Find method of the eventhorizon.ReadRepo interface.
-func (r *Repo) Find(ctx context.Context, id eh.UUID) (eh.Entity, error) {
-	ns := r.namespace(ctx)
+func (r *Repo) Find(ctx context.Context, id eh.UUID) (ret eh.Entity, err error) {
+	r.db.View(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte(eh.NamespaceFromContext(ctx)))
+		if b == nil {
+			// bucket doesnt exist: we dont care.
+			return nil
+		}
+		v := b.Get([]byte(id))
+		if v == nil {
+			// entity doesn't exist: we dont care
+			return nil
+		}
+		decoder := gob.NewDecoder(bytes.NewBuffer(v))
+		err := decoder.Decode(&ret)
+		if err != nil {
+			// couldnt decode it: academically interesting... why?
+			fmt.Println("decode fail: ", err)
+		}
+		return nil
+	})
 
-	r.dbMu.RLock()
-	defer r.dbMu.RUnlock()
-	model, ok := r.db[ns][id]
-	if !ok {
+	if ret == nil {
 		return nil, eh.RepoError{
 			Err:       eh.ErrEntityNotFound,
 			Namespace: eh.NamespaceFromContext(ctx),
 		}
 	}
 
-	return model, nil
+	return ret, nil
 }
 
 // FindAll implements the FindAll method of the eventhorizon.ReadRepo interface.
-func (r *Repo) FindAll(ctx context.Context) ([]eh.Entity, error) {
-	ns := r.namespace(ctx)
-
-	r.dbMu.RLock()
-	defer r.dbMu.RUnlock()
-	all := []eh.Entity{}
-	for _, id := range r.ids[ns] {
-		if m, ok := r.db[ns][id]; ok {
-			all = append(all, m)
+func (r *Repo) FindAll(ctx context.Context) (ret []eh.Entity, err error) {
+	var model eh.Entity
+	r.db.View(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte(eh.NamespaceFromContext(ctx)))
+		if b == nil {
+			return nil
 		}
-	}
-
-	return all, nil
+		b.ForEach(func(k, v []byte) error {
+			decoder := gob.NewDecoder(bytes.NewBuffer(v))
+			err := decoder.Decode(&model)
+			if err != nil {
+				fmt.Println("decode fail: ", err)
+				return nil
+			}
+			ret = append(ret, model)
+			return nil
+		})
+		return nil
+	})
+	return ret, nil
 }
+
+var ErrEntityEncodeError = errors.New("error encoding entity")
 
 // Save implements the Save method of the eventhorizon.WriteRepo interface.
 func (r *Repo) Save(ctx context.Context, entity eh.Entity) error {
-	ns := r.namespace(ctx)
-
 	if entity.EntityID() == eh.UUID("") {
 		return eh.RepoError{
 			Err:       eh.ErrCouldNotSaveEntity,
@@ -97,60 +102,60 @@ func (r *Repo) Save(ctx context.Context, entity eh.Entity) error {
 		}
 	}
 
-	r.dbMu.Lock()
-	defer r.dbMu.Unlock()
-	id := entity.EntityID()
-	if _, ok := r.db[ns][id]; !ok {
-		r.ids[ns] = append(r.ids[ns], id)
+	//encode entity
+	encodedEntity := new(bytes.Buffer)
+	encoder := gob.NewEncoder(encodedEntity)
+	err := encoder.Encode(&entity)
+	if err != nil {
+		fmt.Println("error in encoding Entity", err)
+		return eh.RepoError{
+			Err:       eh.ErrCouldNotSaveEntity,
+			BaseErr:   ErrEntityEncodeError,
+			Namespace: eh.NamespaceFromContext(ctx),
+		}
 	}
-	r.db[ns][id] = entity
+
+	r.db.Update(func(tx *bbolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte(eh.NamespaceFromContext(ctx)))
+		if err != nil {
+			fmt.Println("Bucket Not Created: ", err)
+			return nil
+		}
+
+		err = b.Put([]byte(entity.EntityID()), encodedEntity.Bytes())
+		if err != nil {
+			fmt.Println("Save error:", err)
+		}
+
+		return err
+	})
 
 	return nil
 }
 
-// Remove implements the Remove method of the eventhorizon.WriteRepo interface.
 func (r *Repo) Remove(ctx context.Context, id eh.UUID) error {
-	ns := r.namespace(ctx)
-
-	r.dbMu.Lock()
-	defer r.dbMu.Unlock()
-	if _, ok := r.db[ns][id]; ok {
-		delete(r.db[ns], id)
-
-		index := -1
-		for i, d := range r.ids[ns] {
-			if id == d {
-				index = i
-				break
-			}
+	r.db.Update(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte(eh.NamespaceFromContext(ctx)))
+		if b == nil {
+			return nil
 		}
-		r.ids[ns] = append(r.ids[ns][:index], r.ids[ns][index+1:]...)
 
-		return nil
-	}
-
-	return eh.RepoError{
-		Err:       eh.ErrEntityNotFound,
-		Namespace: eh.NamespaceFromContext(ctx),
-	}
+		err := b.Delete([]byte(id))
+		if err != nil {
+			fmt.Println("Delete error", err)
+		}
+		return err
+	})
+	return nil
 }
 
-// Helper to get the namespace and ensure that its data exists.
 func (r *Repo) namespace(ctx context.Context) namespace {
-	ns := namespace(eh.NamespaceFromContext(ctx))
-
-	r.dbMu.Lock()
-	defer r.dbMu.Unlock()
-	if _, ok := r.db[ns]; !ok {
-		r.db[ns] = map[eh.UUID]eh.Entity{}
-		r.ids[ns] = []eh.UUID{}
-	}
-
-	return ns
+	return namespace(eh.NamespaceFromContext(ctx))
 }
 
 // Repository returns a parent ReadRepo if there is one.
 func Repository(repo eh.ReadRepo) *Repo {
+	fmt.Println("Repository function called")
 	if repo == nil {
 		return nil
 	}
