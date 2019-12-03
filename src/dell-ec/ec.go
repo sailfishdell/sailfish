f605a925c1548038d7d517233b18d49aa3380e0a
michael_e_brown <michael_e_brown@dell.com>
2019-12-03 08:47:27 -0600

    BITS0000: checktree is too slow to run on production system. Need to work a different way to do this task without locking the entire tree.
    Change-Id: I2b97b44b64b9cd3bc56c254633b5e14781b1971c

--- src/dell-ec/ec.go
+++ src/dell-ec/ec.go
@@ -116,7 +116,8 @@ func New(ctx context.Context, logger log.Logger, cfgMgr *viper.Viper, cfgMgrMu *
 	godefs.InitGoDef()
 
 	// start background tree checking
-	go d.CheckTree()
+	// disable for now
+	//go d.CheckTree()
 
 	awesome_mapper2.AddFunction("find_uris_with_basename", func(args ...interface{}) (interface{}, error) {
 		if len(args) < 1 {
