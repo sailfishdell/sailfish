62068e0c363992f7d2bf4ecf035f2301fc8f0253
michael_e_brown <michael_e_brown@dell.com>
2019-12-19 16:49:52 +0000

    BITS0000: metric-engine: add feature to import metrics by column from UDB.
    Change-Id: I110810bb7f2289814605276919b4c44bcab26ab1

--- cmd/metric-engine/udb/udb.go
+++ cmd/metric-engine/udb/udb.go
@@ -3,6 +3,7 @@ package udb
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -35,8 +36,9 @@ func NewUDBFactory(logger log.Logger, database *sqlx.DB, d BusComponents, cfg *v
 	ret.udb = map[string]UDBMeta{}
 
 	impFns := map[string]func(string, ...string) error{
-		"DirectMetric": func(n string, args ...string) error { return ret.ImportByMetricValue(n, args...) },
-		"Error":        func(n string, args ...string) error { return ret.ImportERROR(n, args...) },
+		"DirectMetric":  func(n string, args ...string) error { return ret.ImportByMetricValue(n, args...) },
+		"MetricColumns": func(n string, args ...string) error { return ret.ImportByColumn(n, args...) },
+		"Error":         func(n string, args ...string) error { return ret.ImportERROR(n, args...) },
 	}
 
 	for k, v := range cfg.GetStringMap("UDB-Metric-Import") {
@@ -86,7 +88,7 @@ func (l *UDBFactory) ConditionalImport(udbImportName string) (err error) {
 		return xerrors.Errorf("UDB table %s not set up in meta struct", udbImportName)
 	}
 
-	if time.Now().Unix()%meta.interval != 0 {
+	if meta.interval == 0 || time.Now().Unix()%meta.interval != 0 {
 		return
 	}
 
@@ -118,6 +120,85 @@ func (l *UDBFactory) IterUDBTables(fn func(string) error) error {
 }
 
 const maximport = 50
+
+// ImportByColumn will import a database rows where each column is a different metric.
+//   Each column that is a metric has to have its column name prefixed with "Metric-"
+//   Timestamps are constructed based on the "Timestamp" column
+//   Metric Context is constructed based on the "Context" column
+//   Property paths are constructed by appending '#<metricname>' to the "Property" column
+func (l *UDBFactory) ImportByColumn(udbImportName string, args ...string) (err error) {
+	err = nil
+	events := []eh.EventData{}
+	defer func() {
+		if err == nil {
+			if len(events) > 0 {
+				fmt.Printf("Emitted %d events for table %s.\n", len(events), udbImportName)
+			}
+		} else {
+			fmt.Printf("Emitted %d events for table %s. (err: %s)\n", len(events), udbImportName, err)
+		}
+	}()
+
+	udbMeta, ok := l.udb[udbImportName]
+	if !ok {
+		err = xerrors.Errorf("Somehow got called with a table name not in our supported tables list.")
+		return
+	}
+
+	if udbMeta.query == nil {
+		err = xerrors.Errorf("SQL query wasn't prepared for %s!", udbImportName)
+		return
+	}
+
+	rows, err := udbMeta.query.Queryx()
+	if err != nil {
+		err = xerrors.Errorf("query failed for %s: %w", udbImportName, err)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		mm := map[string]interface{}{}
+		err = rows.MapScan(mm)
+		if err != nil {
+			l.logger.Crit("Error scanning row into MetricEvent", "err", err, "udbImportName", udbImportName)
+			continue
+		}
+
+		ts := mm["Timestamp"].(int64)
+		delete(mm, "Timestamp")
+		metricCtx := string(mm["Context"].([]byte))
+		delete(mm, "Context")
+		property := string(mm["Property"].([]byte))
+		delete(mm, "Property")
+
+		for k, v := range mm {
+			event := &MetricValueEventData{Timestamp: SqlTimeInt{time.Unix(0, ts)}, Context: metricCtx}
+			if !strings.HasPrefix(k, "Metric-") {
+				fmt.Printf("Skipping %s\n", k)
+				continue
+			}
+			if v == nil {
+				fmt.Printf("Skipping nil Metric: %s\n", k)
+				continue
+			}
+			event.Name = k[7:]
+			event.Value = fmt.Sprintf("%v", v)
+			event.Property = property + "#" + k[7:]
+
+			events = append(events, event)
+			if len(events) > maximport {
+				l.bus.PublishEvent(context.Background(), eh.NewEvent(MetricValueEvent, events, time.Now()))
+				events = []eh.EventData{}
+			}
+		}
+	}
+	if len(events) > 0 {
+		l.bus.PublishEvent(context.Background(), eh.NewEvent(MetricValueEvent, events, time.Now()))
+	}
+
+	return nil
+}
 
 func (l *UDBFactory) ImportByMetricValue(udbImportName string, args ...string) (err error) {
 	err = nil
@@ -164,7 +245,9 @@ func (l *UDBFactory) ImportByMetricValue(udbImportName string, args ...string) (
 			events = []eh.EventData{}
 		}
 	}
-	l.bus.PublishEvent(context.Background(), eh.NewEvent(MetricValueEvent, events, time.Now()))
+	if len(events) > 0 {
+		l.bus.PublishEvent(context.Background(), eh.NewEvent(MetricValueEvent, events, time.Now()))
+	}
 
 	return nil
 }
