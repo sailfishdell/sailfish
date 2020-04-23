42bae81dfb8851e9e6bc462340a5bb5ccfc82ae9
michael_e_brown <michael_e_brown@dell.com>
2020-04-23 16:04:38 -0500

    BITS0000:  event waiter, BUGFIX and helpers 	- add some helpers for working with sync events 	- bugfix where we were not properly syncing when splitting 	  []eh.EventData and returning early from sync events         eventwaiter - NO-OP - address line length linter issues on comments
    Change-Id: Ie463009b8c01ea3784aef8f6de635ee0b4f9b176

--- src/looplab/event/event.go
+++ src/looplab/event/event.go
@@ -1,11 +1,17 @@
 package event
 
 import (
+	"context"
 	"sync"
 	"time"
 
 	eh "github.com/looplab/eventhorizon"
 )
+
+// drop-in replacement for eh.NewEvent
+func NewEvent(t eh.EventType, d eh.EventData, s time.Time) eh.Event {
+	return eh.NewEvent(t, d, s)
+}
 
 type SyncEvent struct {
 	eh.Event
@@ -17,4 +23,36 @@ func NewSyncEvent(t eh.EventType, d eh.EventData, s time.Time) SyncEvent {
 		Event:     eh.NewEvent(t, d, s),
 		WaitGroup: &sync.WaitGroup{},
 	}
+}
+
+func PrepSyncEvent(t eh.EventType, d eh.EventData, s time.Time) SyncEvent {
+	evt := NewSyncEvent(t, d, s)
+	evt.Add(1)
+	return evt
+}
+
+type syncEvent interface {
+	Add(int)
+	Done()
+}
+
+func PinSyncEvent(evt eh.Event) {
+	if e, ok := evt.(syncEvent); ok {
+		e.Add(1)
+	}
+}
+
+func ReleaseSyncEvent(evt eh.Event) {
+	if e, ok := evt.(syncEvent); ok {
+		e.Done()
+	}
+}
+
+func PublishAndWait(ctx context.Context, bus eh.EventBus, t eh.EventType, d eh.EventData) {
+	evt := PrepSyncEvent(t, d, time.Now())
+	err := bus.PublishEvent(ctx, evt)
+	if err != nil {
+		return
+	}
+	evt.Wait()
 }
